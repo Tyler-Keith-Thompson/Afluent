b1d007ea826a6f05a27c539943e499d519177029
Tyler Thompson <Tyler.Keith.Thompson@gmail.com>
2023-10-29 01:35:39 -0600

    [main] - Raise macOS version to 13 - TT

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "Afluent",
-    platforms: [.iOS(.v15), .macOS(.v12)],
+    platforms: [.iOS(.v15), .macOS(.v13)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(

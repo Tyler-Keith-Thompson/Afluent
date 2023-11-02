f32cfdc54db389333d0bb6cf879e3a01aa54c55c
Tyler Thompson <Tyler.Keith.Thompson@gmail.com>
2023-11-01 22:50:22 -0600

    [breakpoint] - Add breakpoint operator - TT

---
+++ Sources/Afluent/Workers/Breakpoint.swift
@@ -0,0 +1,42 @@
+//
+//  Breakpoint.swift
+//
+//
+//  Created by Tyler Thompson on 11/1/23.
+//
+
+import Foundation
+import Combine
+
+extension AsynchronousUnitOfWork {
+    /// Introduces a breakpoint into the asynchronous unit of work.
+    ///
+    /// This function allows you to introduce conditional breakpoints based on the output or error of the asynchronous operation.
+    /// If the provided conditions are met, a `SIGTRAP` signal is raised, pausing execution in a debugger.
+    ///
+    /// - Parameters:
+    ///   - receiveOutput: A closure that takes the successful output of the operation. If this closure returns `true`, a breakpoint is triggered. Default is `nil`.
+    ///   - receiveError: A closure that takes any error produced by the operation. If this closure returns `true`, a breakpoint is triggered. Default is `nil`.
+    ///
+    /// - Returns: An asynchronous unit of work with the breakpoint conditions applied.
+    @_transparent @_alwaysEmitIntoClient @inlinable public func breakpoint(receiveOutput: ((Success) async throws -> Bool)? = nil, receiveError: ((Error) async throws -> Bool)? = nil) -> some AsynchronousUnitOfWork<Success> {
+        handleEvents { output in
+            if try await receiveOutput?(output) == true {
+                raise(SIGTRAP)
+            }
+        } receiveError: { error in
+            if try await receiveError?(error) == true {
+                raise(SIGTRAP)
+            }
+        }
+    }
+    
+    /// Introduces a breakpoint into the asynchronous unit of work when an error occurs.
+    ///
+    /// This function triggers a `SIGTRAP` signal, pausing execution in a debugger, whenever the asynchronous operation produces an error.
+    ///
+    /// - Returns: An asynchronous unit of work with the breakpoint-on-error condition applied.
+    @_transparent @_alwaysEmitIntoClient @inlinable public func breakpointOnError() -> some AsynchronousUnitOfWork<Success> {
+        breakpoint(receiveError: { _ in true })
+    }
+}

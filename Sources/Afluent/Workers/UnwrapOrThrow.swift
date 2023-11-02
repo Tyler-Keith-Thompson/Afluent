ec61898ea98b54481e44c990b7e3cf6b5c42efd8
Tyler Thompson <33705774+Tyler-Keith-Thompson@users.noreply.github.com>
2023-11-02 09:47:52 -0600

    Added unwrapOrThrow operator (#26)

---
+++ Sources/Afluent/Workers/UnwrapOrThrow.swift
@@ -0,0 +1,22 @@
+//
+//  UnwrapOrThrow.swift
+//
+//
+//  Created by Tyler Thompson on 11/2/23.
+//
+
+import Foundation
+
+extension AsynchronousUnitOfWork {
+    /// Unwraps the optional value if present, or throws an error.
+    public func unwrap<T>(orThrow error: @escaping @autoclosure () -> Error) -> some AsynchronousUnitOfWork<T> where Success == T? {
+        tryMap { output in
+            switch output {
+            case .some(let value):
+                return value
+            case nil:
+                throw error()
+            }
+        }
+    }
+}
---
+++ Tests/AfluentTests/WorkerTests/UnwrapOrThrowTests.swift
@@ -0,0 +1,42 @@
+//
+//  UnwrapOrThrowTests.swift
+//
+//
+//  Created by Tyler Thompson on 11/2/23.
+//
+
+import Foundation
+import Afluent
+import XCTest
+
+final class UnwrapOrThrowTests: XCTestCase {
+    func testUnwrapThrowsErrorIfOptionalIsNone() async throws {
+        enum Err: Error {
+            case e1
+        }
+        
+        let result = try await DeferredTask {
+            nil as Int?
+        }
+        .unwrap(orThrow: Err.e1)
+        .result
+        
+        XCTAssertThrowsError(try result.get()) { error in
+            XCTAssertEqual(error as? Err, .e1)
+        }
+    }
+    
+    func testUnwrapThrowsErrorIfOptionalIsSome() async throws {
+        enum Err: Error {
+            case e1
+        }
+        
+        let result = try await DeferredTask {
+            1 as Int?
+        }
+        .unwrap(orThrow: Err.e1)
+        .result
+        
+        XCTAssertEqual(try result.get(), 1)
+    }
+}

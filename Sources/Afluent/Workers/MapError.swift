8e40332721e00b24b4a095fbd27f7fff24e8aaf4
Tyler Thompson <Tyler.Keith.Thompson@gmail.com>
2023-11-03 20:53:05 -0600

    [map-specific-error] - Added docs - TT

--- Sources/Afluent/Workers/MapError.swift
+++ Sources/Afluent/Workers/MapError.swift
@@ -36,6 +36,15 @@ extension AsynchronousUnitOfWork {
         Workers.MapError(upstream: self, transform: transform)
     }
     
+    /// Transforms a specific error produced by the asynchronous unit of work.
+    ///
+    /// This function allows you to modify or replace a specific error produced by the current unit of work. If the error produced matches the provided error, the transform closure is applied; otherwise, the original error is propagated unchanged.
+    ///
+    /// - Parameters:
+    ///   - error: The specific error to be transformed. This error is equatable, allowing for precise matching.
+    ///   - transform: A closure that takes the matched error and returns a transformed error.
+    ///
+    /// - Returns: An asynchronous unit of work that produces either the transformed error (if a match was found) or the original error.
     public func mapError<E: Error & Equatable>(_ error: E, _ transform: @escaping (Error) -> Error) -> some AsynchronousUnitOfWork<Success> {
         mapError {
             if let e = $0 as? E, e == error { return transform(e) }

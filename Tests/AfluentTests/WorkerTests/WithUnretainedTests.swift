f6923d58842f4f0d2179c1fbfbe4bbfb02453b01
Daniel Bachar <danielbac@zillowgroup.com>
2023-11-26 14:57:35 -0700

    Extension of AsynchronousUnitOfWork - WithUnretained.

---
+++ Tests/AfluentTests/WorkerTests/WithUnretainedTests.swift
@@ -0,0 +1,38 @@
+//
+//  WithUnretainedTests.swift
+//
+//
+//  Created by Daniel Bachar on 11/8/23.
+//
+import Afluent
+
+import Foundation
+import XCTest
+
+
+final class WithUnretainedTests: XCTestCase {
+    class MyType {}
+    
+    func testWithUnretainedHolds() async throws {
+        
+        let myTypeInstance = MyType()
+
+        try await DeferredTask { 1 }
+            .withUnretained(myTypeInstance, resultSelector: { myType, _ in
+                XCTAssertNotNil(myType)
+            })
+            .execute()
+    }
+    
+    func testWithUnretainedThrows() async throws {
+        do {
+            try await DeferredTask { 1 }
+                .withUnretained(MyType(), resultSelector: { _, _ in })
+                .execute()
+            XCTFail("Should throw if failed to retain")
+        } catch {
+            XCTAssertEqual(error as? UnretainedError, UnretainedError.failedRetaining)
+        }
+        
+    }
+}

2a0282a55249abfcec14f941128b10dd796a2afe
Tyler Thompson <Tyler.Keith.Thompson@gmail.com>
2023-12-01 08:53:40 -0700

    [materialize-sequence] - Added materialize operator - TT

---
+++ Sources/Afluent/SequenceOperators/MaterializeSequence.swift
@@ -0,0 +1,63 @@
+//
+//  MaterializeSequence.swift
+//
+//
+//  Created by Tyler Thompson on 12/1/23.
+//
+
+import Foundation
+
+extension AsyncSequences {
+    public struct Materialize<Upstream: AsyncSequence>: AsyncSequence {
+        public typealias Element = Event
+        
+        let upstream: Upstream
+        
+        /// Represents the different kinds of events that can be emitted by `Materialize`.
+        public enum Event {
+            /// An element from the upstream sequence.
+            case element(Upstream.Element)
+            /// An error encountered in the upstream sequence.
+            case failure(Error)
+            /// The completion of the upstream sequence.
+            case complete
+        }
+        
+        public struct AsyncIterator: AsyncIteratorProtocol {
+            let upstream: Upstream
+            var completed = false
+            lazy var iterator = upstream.makeAsyncIterator()
+            
+            public mutating func next() async throws -> Element? {
+                guard !completed else { return nil }
+                do {
+                    try Task.checkCancellation()
+                    if let val = try await iterator.next() {
+                        return .element(val)
+                    } else {
+                        completed = true
+                        return .complete
+                    }
+                } catch {
+                    guard !(error is CancellationError) else { throw error }
+                    return .failure(error)
+                }
+            }
+        }
+        
+        public func makeAsyncIterator() -> AsyncIterator {
+            AsyncIterator(upstream: upstream)
+        }
+    }
+}
+
+extension AsyncSequence {
+    /// Transforms the elements, completion, and errors of the current `AsyncSequence` into `Event` values.
+    ///
+    /// This method wraps the `AsyncSequence` and emits each of its elements, errors, and completion as distinct `Event` cases. It's useful for uniformly handling all aspects of the sequence's lifecycle.
+    ///
+    /// - Returns: An `AsyncSequences.Materialize` instance that represents the transformed sequence.
+    public func materialize() -> AsyncSequences.Materialize<Self> {
+        AsyncSequences.Materialize(upstream: self)
+    }
+}
---
+++ Tests/AfluentTests/SequenceTests/MaterializeSequenceTests.swift
@@ -0,0 +1,90 @@
+//
+//  MaterializeSequenceTests.swift
+//
+//
+//  Created by Tyler Thompson on 12/1/23.
+//
+
+import Foundation
+import Afluent
+import XCTest
+
+final class MaterializeSequenceTests: XCTestCase {
+    func testMaterializeCapturesSuccesses() async throws {
+        let result = try await DeferredTask { 1 }
+            .toAsyncSequence()
+            .materialize()
+            .first()
+        
+        if case .element(let val) = result {
+            XCTAssertEqual(val, 1)
+        } else {
+            XCTFail("Expected element, got: \(String(describing: result))")
+        }
+    }
+    
+    func testMaterializeCapturesCompletion() async throws {
+        let result = try await DeferredTask { 1 }
+            .toAsyncSequence()
+            .materialize()
+            .dropFirst()
+            .first()
+
+        guard case .complete = result else {
+            XCTFail("Expected completion, got: \(String(describing: result))")
+            return
+        }
+    }
+    
+    func testMaterializeCapturesNonCancelErrors() async throws {
+        let result = try await DeferredTask { throw URLError(.badURL) }
+            .toAsyncSequence()
+            .materialize()
+            .first()
+        
+        if case .failure(let error) = result {
+            XCTAssertEqual(error as? URLError, URLError(.badURL))
+        } else {
+            XCTFail("Expected failure, got: \(String(describing: result))")
+        }
+    }
+    
+    func testMaterializeDoesNotInterfereWithCancellation() async throws {
+        try XCTSkipIf(ProcessInfo.processInfo.environment["CI"] == "true")
+        actor Test {
+            var started = false
+            var ended = false
+            
+            func start() { started = true }
+            func end() { ended = true }
+        }
+        let test = Test()
+        
+        let exp = self.expectation(description: "thing happened")
+        exp.isInverted = true
+        let task = Task {
+            try await DeferredTask {
+                await test.start()
+                try await Task.sleep(for: .milliseconds(10))
+            }.map {
+                await test.end()
+                exp.fulfill()
+            }
+            .toAsyncSequence()
+            .materialize()
+            .first()
+        }
+        
+        try await Task.sleep(for: .milliseconds(2))
+        
+        task.cancel()
+        
+        await fulfillment(of: [exp], timeout: 0.011)
+        
+        let started = await test.started
+        let ended = await test.ended
+        
+        XCTAssert(started)
+        XCTAssertFalse(ended)
+    }
+}

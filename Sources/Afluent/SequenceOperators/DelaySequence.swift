20b66e779eb7fb41a2de35d9d8b8ae0a0c64a57d
Tyler Thompson <33705774+Tyler-Keith-Thompson@users.noreply.github.com>
2023-12-09 14:59:37 -0700

    Delay Sequence Operator (#57)
    Added a delay sequence operator that mimics Combine's behavior.

---
+++ Sources/Afluent/SequenceOperators/DelaySequence.swift
@@ -0,0 +1,76 @@
+//
+//  DelaySequence.swift
+//
+//
+//  Created by Tyler Thompson on 12/8/23.
+//
+
+import Foundation
+
+extension AsyncSequences {
+    public struct Delay<Upstream: AsyncSequence, C: Clock>: AsyncSequence {
+        public typealias Element = Upstream.Element
+        let upstream: Upstream
+        let interval: C.Duration
+        let clock: C
+        let tolerance: C.Duration?
+
+        public struct AsyncIterator: AsyncIteratorProtocol {
+            typealias Instant = C.Instant
+            let upstream: Upstream
+            let interval: C.Duration
+            var iterator: AsyncThrowingStream<(Instant, Element), Error>.Iterator
+            let clock: C
+            let tolerance: C.Duration?
+
+            init(upstream: Upstream, interval: C.Duration, clock: C, tolerance: C.Duration?) {
+                self.upstream = upstream
+                self.interval = interval
+                self.clock = clock
+                self.tolerance = tolerance
+                let stream = AsyncThrowingStream<(Instant, Element), Error> { continuation in
+                    Task {
+                        do {
+                            for try await el in upstream {
+                                continuation.yield((clock.now, el))
+                            }
+                            continuation.finish()
+                        } catch {
+                            continuation.finish(throwing: error)
+                        }
+                    }
+                }
+                self.iterator = stream.makeAsyncIterator()
+            }
+            
+            public mutating func next() async throws -> Element? {
+                try Task.checkCancellation()
+                if let (instant, element) = try await iterator.next() {
+                    let suspensionPoint = instant.advanced(by: interval)
+                    try await clock.sleep(until: suspensionPoint, tolerance: tolerance)
+                    return element
+                }
+                return nil
+            }
+        }
+        
+        
+        public func makeAsyncIterator() -> AsyncIterator {
+            AsyncIterator(upstream: upstream, interval: interval, clock: clock, tolerance: tolerance)
+        }
+    }
+}
+
+extension AsyncSequence {
+    /// Delays delivery of all output to the downstream receiver by a specified amount of time
+    /// - Parameter interval: The amount of time to delay.
+    public func delay(for interval: Measurement<UnitDuration>, tolerance: Measurement<UnitDuration>? = nil) -> AsyncSequences.Delay<Self, SuspendingClock> {
+        delay(for: .nanoseconds(UInt(interval.converted(to: .nanoseconds).value)), tolerance: tolerance.flatMap { .nanoseconds(UInt($0.converted(to: .nanoseconds).value)) }, clock: SuspendingClock())
+    }
+    
+    /// Delays delivery of all output to the downstream receiver by a specified amount of time
+    /// - Parameter interval: The amount of time to delay.
+    public func delay<C: Clock>(for interval: C.Duration, tolerance: C.Duration? = nil, clock: C) -> AsyncSequences.Delay<Self, C> {
+        AsyncSequences.Delay(upstream: self, interval: interval, clock: clock, tolerance: tolerance)
+    }
+}
---
+++ Tests/AfluentTests/SequenceTests/DelaySequenceTests.swift
@@ -0,0 +1,110 @@
+//
+//  DelaySequenceTests.swift
+//
+//
+//  Created by Tyler Thompson on 12/8/23.
+//
+
+import Foundation
+import Afluent
+import XCTest
+
+final class DelaySequenceTests: XCTestCase {
+    func testDelay_DelaysAllOutputByExpectedTime() async throws {
+        // Create a simple AsyncSequence of integers
+        let numbers = [1, 2, 3].async
+        let delayDuration = Measurement(value: 10, unit: UnitDuration.milliseconds)
+
+        // Measure the time before starting the delayed sequence
+        let startTime = Date()
+
+        // Create the delayed sequence
+        let delayedNumbers = numbers.delay(for: delayDuration)
+
+        // Iterate over the delayed sequence
+        var count = 0
+        for try await _ in delayedNumbers {
+            count += 1
+            let currentTime = Date()
+            let elapsedTime = currentTime.timeIntervalSince(startTime)
+
+            // Check if the elapsed time is approximately equal to the expected delay
+            let expectedDelay = delayDuration.converted(to: .seconds).value
+            XCTAssertGreaterThan(elapsedTime, expectedDelay, "Element \(count) was not delayed correctly.")
+        }
+
+        // Ensure all elements were received
+        XCTAssertEqual(count, 3, "Not all elements were received.")
+    }
+    
+    func testDelay_DoesNotDelayEveryElement() async throws {
+        // Create a simple AsyncSequence of integers
+        let numbers = [1, 2, 3].async
+        let delayDuration = Measurement(value: ProcessInfo.processInfo.environment["CI"] == "true" ? 100 : 10, unit: UnitDuration.milliseconds)
+
+        // Measure the time before starting the delayed sequence
+        let startTime = Date()
+
+        // Create the delayed sequence
+        let delayedNumbers = numbers.delay(for: delayDuration)
+
+        // Iterate over the delayed sequence
+        var count = 0
+        for try await _ in delayedNumbers {
+            count += 1
+            let currentTime = Date()
+            let elapsedTime = currentTime.timeIntervalSince(startTime)
+
+            if count == 1 {
+                let expectedDelay = delayDuration.converted(to: .seconds).value
+                XCTAssertGreaterThan(elapsedTime, expectedDelay, "Element \(count) was not delayed correctly.")
+            } else {
+                let expectedDelay = delayDuration.converted(to: .seconds).value * Double(count)
+                XCTAssertLessThan(elapsedTime, expectedDelay, "Element \(count) was not delayed correctly.")
+            }
+        }
+
+        // Ensure all elements were received
+        XCTAssertEqual(count, 3, "Not all elements were received.")
+    }
+    
+    func testDelay_DelaysCorrectlyEvenAfterIntervalHasPassed() async throws {
+        let stream = AsyncStream<Int> { continuation in
+            DeferredTask { continuation.yield(1) }
+                .delay(for: .milliseconds(15))
+                .map { continuation.yield(2); continuation.finish() }
+                .run()
+        }
+
+        let delayedNumbers = stream.delay(for: .milliseconds(10))
+        
+        let startTime = Date()
+
+        // Iterate over the delayed sequence
+        var count = 0
+        for try await _ in delayedNumbers {
+            count += 1
+            let currentTime = Date()
+            let elapsedTime = currentTime.timeIntervalSince(startTime)
+
+            if count == 1 {
+                let expectedDelay = Measurement<UnitDuration>.milliseconds(10).converted(to: .seconds).value
+                XCTAssertGreaterThan(elapsedTime, expectedDelay, "Element \(count) was not delayed correctly.")
+            } else {
+                let expectedDelay = Measurement<UnitDuration>.milliseconds(15 + 10).converted(to: .seconds).value
+                XCTAssertGreaterThan(elapsedTime, expectedDelay, "Element \(count) was not delayed correctly.")
+            }
+        }
+    }
+}
+
+extension Array {
+    fileprivate var async: AsyncStream<Element> {
+        AsyncStream { continuation in
+            for element in self {
+                continuation.yield(element)
+            }
+            continuation.finish()
+        }
+    }
+}

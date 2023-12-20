4ea44b357ea86131e4f528c73aeb167271d7af9f
Annalise Mariottini <annalise.mariottini@columbia.edu>
2023-12-20 16:50:20 -0500

    rename in print too

--- Sources/Afluent/Workers/Print.swift
+++ Sources/Afluent/Workers/Print.swift
@@ -16,7 +16,7 @@ extension AsynchronousUnitOfWork {
     /// - Returns: An `AsynchronousUnitOfWork` that behaves identically to the upstream but logs events.
     public func print(_ prefix: String = "") -> some AsynchronousUnitOfWork<Success> {
         handleEvents {
-            Swift.print("\(prefix) received execute")
+            Swift.print("\(prefix) received operation")
         } receiveOutput: {
             Swift.print("\(prefix) received output: \($0)")
         } receiveError: {

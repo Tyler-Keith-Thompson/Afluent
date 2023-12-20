ad1c97fa96b7860539cc3fb8860ca14e004dec60
Tyler Thompson <33705774+Tyler-Keith-Thompson@users.noreply.github.com>
2023-12-19 17:58:01 -0700

    Added print, replaceError, and replaceNil operators - TT (#75)

---
+++ Sources/Afluent/SequenceOperators/ReplaceErrorSequence.swift
@@ -0,0 +1,47 @@
+//
+//  ReplaceErrorSequence.swift
+//
+//
+//  Created by Tyler Thompson on 12/17/23.
+//
+
+import Foundation
+
+extension AsyncSequences {
+    public struct ReplaceError<Upstream: AsyncSequence, Output>: AsyncSequence where Upstream.Element == Output {
+        public typealias Element = Upstream.Element
+        let upstream: Upstream
+        let newOutput: Output
+
+        public struct AsyncIterator: AsyncIteratorProtocol {
+            var upstreamIterator: Upstream.AsyncIterator
+            let newOutput: Output
+
+            public mutating func next() async throws -> Element? {
+                do {
+                    try Task.checkCancellation()
+                    return try await upstreamIterator.next()
+                } catch {
+                    guard !(error is CancellationError) else { throw error }
+                    return newOutput
+                }
+            }
+        }
+
+        public func makeAsyncIterator() -> AsyncIterator {
+            AsyncIterator(upstreamIterator: upstream.makeAsyncIterator(),
+                          newOutput: newOutput)
+        }
+    }
+}
+
+extension AsyncSequence {
+    /// Replaces any errors from the upstream `AsyncSequence` with the provided value.
+    ///
+    /// - Parameter value: The value to emit upon encountering an error.
+    ///
+    /// - Returns: An `AsyncSequence` that emits the specified value instead of failing when the upstream fails.
+    public func replaceError(with value: Element) -> AsyncSequences.ReplaceError<Self, Element> {
+        AsyncSequences.ReplaceError(upstream: self, newOutput: value)
+    }
+}
---
+++ Sources/Afluent/SequenceOperators/ReplaceNilSequence.swift
@@ -0,0 +1,46 @@
+//
+//  ReplaceNilSequence.swift
+//
+//
+//  Created by Tyler Thompson on 12/17/23.
+//
+
+import Foundation
+
+extension AsyncSequences {
+    public struct ReplaceNil<Upstream: AsyncSequence, Output>: AsyncSequence where Upstream.Element == Output? {
+        public typealias Element = Upstream.Element
+        let upstream: Upstream
+        let newOutput: Output
+
+        public struct AsyncIterator: AsyncIteratorProtocol {
+            var upstreamIterator: Upstream.AsyncIterator
+            let newOutput: Output
+
+            public mutating func next() async throws -> Element? {
+                try Task.checkCancellation()
+                switch try await upstreamIterator.next() {
+                    case .none: return nil
+                    case .some(.none): return newOutput
+                    case .some(.some(let output)): return output
+                }
+            }
+        }
+
+        public func makeAsyncIterator() -> AsyncIterator {
+            AsyncIterator(upstreamIterator: upstream.makeAsyncIterator(),
+                          newOutput: newOutput)
+        }
+    }
+}
+
+extension AsyncSequence {
+    /// Replaces any `nil` values from the upstream `AsyncSequence` with the provided non-nil value.
+    ///
+    /// - Parameter value: The value to emit when the upstream emits `nil`.
+    ///
+    /// - Returns: An `AsyncSequence` that emits the specified value instead of `nil` when the upstream emits `nil`.
+    public func replaceNil<E>(with value: E) -> AsyncSequences.ReplaceNil<Self, E> where Element == E? {
+        AsyncSequences.ReplaceNil(upstream: self, newOutput: value)
+    }
+}

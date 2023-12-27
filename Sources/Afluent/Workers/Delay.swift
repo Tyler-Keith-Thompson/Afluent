238401f862021f826b504e3198dabf3d6b4f464f
Tyler Thompson <Tyler.Keith.Thompson@gmail.com>
2023-12-27 13:20:41 -0700

    [test-stabilization] - Other delay tests fixed - TT

--- Sources/Afluent/Workers/Delay.swift
+++ Sources/Afluent/Workers/Delay.swift
@@ -9,15 +9,17 @@ import Foundation
 
 extension Workers {
     @available(macOS 13.0, iOS 16.0, watchOS 9.0, tvOS 16.0, *)
-    struct Delay<Upstream: AsynchronousUnitOfWork, Success: Sendable>: AsynchronousUnitOfWork where Upstream.Success == Success {
+    struct Delay<Upstream: AsynchronousUnitOfWork, Success: Sendable, C: Clock>: AsynchronousUnitOfWork where Upstream.Success == Success {
         let state = TaskState<Success>()
         let upstream: Upstream
-        let duration: Measurement<UnitDuration>
+        let clock: C
+        let duration: C.Duration
+        let tolerance: C.Duration?
 
         func _operation() async throws -> AsynchronousOperation<Success> {
             AsynchronousOperation {
                 let val = try await upstream.operation()
-                try await Task.sleep(nanoseconds: UInt64(duration.converted(to: .nanoseconds).value))
+                try await clock.sleep(until: clock.now.advanced(by: duration), tolerance: tolerance)
                 return val
             }
         }
@@ -28,15 +30,28 @@ extension AsynchronousUnitOfWork {
     /// Delays the emission of output from the upstream `AsynchronousUnitOfWork` by a specified duration using a given clock.
     ///
     /// - Parameters:
-    ///   - duration: The duration to delay the output, conforming to the `C.Instant.Duration` type.
-    ///   - tolerance: An optional tolerance for the delay. Defaults to `nil`.
-    ///   - clock: The clock used for timekeeping. Defaults to `ContinuousClock()`.
+    ///   - duration: The duration to delay the output.
     ///
     /// - Returns: An `AsynchronousUnitOfWork` that emits the upstream output after the specified delay.
     ///
     /// - Availability: macOS 13.0, iOS 16.0, watchOS 9.0, tvOS 16.0 and above.
     @available(macOS 13.0, iOS 16.0, watchOS 9.0, tvOS 16.0, *)
     public func delay(for duration: Measurement<UnitDuration>) -> some AsynchronousUnitOfWork<Success> {
-        Workers.Delay(upstream: self, duration: duration)
+        Workers.Delay(upstream: self, clock: SuspendingClock(), duration: .nanoseconds(UInt(duration.converted(to: .nanoseconds).value)), tolerance: nil)
+    }
+
+    /// Delays the emission of output from the upstream `AsynchronousUnitOfWork` by a specified duration using a given clock.
+    ///
+    /// - Parameters:
+    ///   - duration: The duration to delay the output, conforming to the `C.Instant.Duration` type.
+    ///   - clock: The clock used for timekeeping. Defaults to `ContinuousClock()`.
+    ///   - tolerance: An optional tolerance for the delay. Defaults to `nil`.
+    ///
+    /// - Returns: An `AsynchronousUnitOfWork` that emits the upstream output after the specified delay.
+    ///
+    /// - Availability: macOS 13.0, iOS 16.0, watchOS 9.0, tvOS 16.0 and above.
+    @available(macOS 13.0, iOS 16.0, watchOS 9.0, tvOS 16.0, *)
+    public func delay<C: Clock>(for duration: C.Duration, clock: C, tolerance: C.Duration?) -> some AsynchronousUnitOfWork<Success> {
+        Workers.Delay(upstream: self, clock: clock, duration: duration, tolerance: tolerance)
     }
 }

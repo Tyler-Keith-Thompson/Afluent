fe0591b4d8a0274c332064b0bd9156543e6c5b9a
Trip Phillips <tripp@zillowgroup.com>
2024-02-16 18:29:56 -0700

    WIP: making progress. all tests pass except for testThrottleDropsAllElementsExceptLastElement_whenThrottledAtDifferentIntervalThanElementsAreReceived_andLatestEqualTrue

--- Sources/Afluent/SequenceOperators/ThrottleSequence.swift
+++ Sources/Afluent/SequenceOperators/ThrottleSequence.swift
@@ -16,18 +16,45 @@ extension AsyncSequences {
         let clock: C
         let latest: Bool
         
+        actor IntervalEvents {
+            var firstElement: Element?
+            var lastElement: Element?
+            var lastElementInstant: C.Instant?
+            var intervalStartInstant: C.Instant?
+            
+            init(firstElement: Element? = nil, 
+                 lastElement: Element? = nil,
+                 lastElementInstant: C.Instant? = nil,
+                 intervalStartInstant: C.Instant? = nil) {
+                self.firstElement = firstElement
+                self.lastElement = lastElement
+                self.lastElementInstant = lastElementInstant
+                self.intervalStartInstant = intervalStartInstant
+            }
+        
+            func updateFirstElement(_ element: Element?, instant: C.Instant) {
+                firstElement = element
+                lastElementInstant = instant
+            }
+            
+            func updateLastElement(_ element: Element?, instant: C.Instant) {
+                lastElement = element
+                lastElementInstant = instant
+            }
+            
+            func updateIntervalStartInstant(_ instant: C.Instant) {
+                intervalStartInstant = instant
+            }
+        }
+        
         public struct AsyncIterator: AsyncIteratorProtocol {
             typealias Instant = C.Instant
             
             let upstream: Upstream
             let interval: C.Duration
-            var iterator: AsyncThrowingStream<(Instant, Element), Error>.Iterator
+            var iterator: AsyncThrowingStream<(Element?, Element?), Error>.Iterator
             let clock: C
             let latest: Bool
-            
-            private var firstIntervalInstant: C.Instant?
-            private var firstElement: Element?
-            private var lastElement: Element?
             
             init(upstream: Upstream,
                  interval: C.Duration,
@@ -37,14 +64,54 @@ extension AsyncSequences {
                 self.interval = interval
                 self.clock = clock
                 self.latest = latest
-                
-                let stream = AsyncThrowingStream<(Instant, Element), Error> { continuation in
+                let stream = AsyncThrowingStream<(Element?, Element?), Error> { continuation in
+                    
+                    // Need a task to run and save the first and last element of all elements from iterators.
+                    
+                    // Need a task to listen for the current time interval.
+                    
+                    
+                    let intervalEvents = IntervalEvents(firstElement: nil, lastElement: nil, intervalStartInstant: clock.now)
+                    
+                    let intervalTask = DeferredTask {
+                        Swift.print("RUN")
+                       
+                            if let intervalStartInstant = await intervalEvents.intervalStartInstant {
+                                let firstElement = await intervalEvents.firstElement
+                                let lastElement = await intervalEvents.lastElement
+                                
+                                let intervalEndInstant = intervalStartInstant.advanced(by: interval)
+                                Swift.print("SLEEP: \(intervalEndInstant)")
+                                
+                                try await clock.sleep(until: intervalEndInstant, tolerance: nil)
+                                
+                                Swift.print("YIELD")
+                                Swift.print("FIRST: \(firstElement)")
+                                Swift.print("LAST: \(lastElement)")
+                                continuation.yield((firstElement, lastElement))
+                                await intervalEvents.updateFirstElement(nil, instant: clock.now)
+                                //await intervalEvents.updateLastElement(nil, instant: clock.now)
+                                
+                                await intervalEvents.updateIntervalStartInstant(clock.now)
+                            } else {
+                                Swift.print("NO START!!")
+                            }
+                    }
+                    
                     Task {
                         do {
                             for try await el in upstream {
-                                let time = clock.now
-                                continuation.yield((time, el))
+                                if await intervalEvents.firstElement == nil {
+                                    intervalTask.run()
+                                    await intervalEvents.updateFirstElement(el, instant: clock.now)
+                                    Swift.print("UPDATE First: \(el)")
+                                }
+                                await intervalEvents.updateLastElement(el, instant: clock.now)
+                                Swift.print("UPDATE Last: \(el)")
                             }
+                            await continuation.yield((intervalEvents.firstElement, intervalEvents.lastElement))
+                            Swift.print("----FINISH----")
+                            intervalTask.cancel()
                             continuation.finish()
                         } catch {
                             continuation.finish(throwing: error)
@@ -56,29 +123,8 @@ extension AsyncSequences {
             
             public mutating func next() async throws -> Element? {
                 try Task.checkCancellation()
-                
-                while let (instant, element) = try await iterator.next() {
-                    if let firstIntervalInstant {
-                        let timeSinceFirstInstant = firstIntervalInstant.duration(to: instant)
-                        if firstElement == nil {
-                            firstElement = element
-                        }
-                        
-                        lastElement = element
-                        
-                        if timeSinceFirstInstant >= interval {
-                            defer {
-                                firstElement = nil
-                            }
-                            self.firstIntervalInstant = instant
-                            return latest ? lastElement : firstElement
-                        } else {
-                            continue
-                        }
-                    } else {
-                        firstIntervalInstant = instant
-                        return element
-                    }
+                while let (first, last) = try await iterator.next() {
+                    return latest ? last : first
                 }
                 return nil
             }
--- Tests/AfluentTests/SequenceTests/ThrottleSequenceTests.swift
+++ Tests/AfluentTests/SequenceTests/ThrottleSequenceTests.swift
@@ -14,38 +14,103 @@ import ConcurrencyExtras
 @available(iOS 16.0, *)
 final class ThrottleSequenceTests: XCTestCase {
     
-    func testThrottleDoesNotDropElements_whenThrottledAtSameIntervalAsElementsAreReceived_andLatestEqualTrue() async throws {
+    func testThrottleDropsAllElementsExceptLast_whenElementsAreReceivedAllAtOnce_andLatestEqualTrue() async throws {
         await withMainSerialExecutor {
             let testClock = TestClock()
             let stream = AsyncStream { continuation in
-                continuation.yield("1")
-                DeferredTask { () }
-                .delay(for: .milliseconds(10))
-                .handleEvents(receiveOutput: { _ in
-                    await testClock.advance(by: .milliseconds(10))
-                    continuation.yield("2")
-                })
-                .delay(for: .milliseconds(10))
-                .handleEvents(receiveOutput: { _ in
-                    await testClock.advance(by: .milliseconds(10))
-                    continuation.yield("3")
-                })
-                .handleEvents(receiveOutput: { _ in
-                    continuation.finish()
-                })
-                .run()
+                continuation.yield(1)
+                continuation.yield(2)
+                continuation.yield(3)
+                continuation.yield(4)
+                continuation.yield(5)
+                continuation.finish()
             }.throttle(for: .milliseconds(10), clock: testClock, latest: true)
             
             let task = Task {
-                var elements = [String]()
+                var elements = [Int]()
                 
                 for try await element in stream {
                     elements.append(element)
                 }
                 
-                XCTAssertEqual(elements, ["1","2","3"])
+                XCTAssertEqual(elements, [5])
             }
-        
+            
+            await testClock.run()
+            
+            _ = await task.result
+        }
+    }
+    
+    func testThrottleDropsAllElementsExceptFirst_whenElementsAreReceivedAllAtOnce_andLatestEqualFalse() async throws {
+        await withMainSerialExecutor {
+            let testClock = TestClock()
+            let stream = AsyncStream { continuation in
+                continuation.yield(1)
+                continuation.yield(2)
+                continuation.yield(3)
+                continuation.yield(4)
+                continuation.yield(5)
+                continuation.finish()
+            }.throttle(for: .milliseconds(10), clock: testClock, latest: false)
+            
+            let task = Task {
+                var elements = [Int]()
+                
+                for try await element in stream {
+                    elements.append(element)
+                }
+                
+                XCTAssertEqual(elements, [1])
+            }
+            
+            await testClock.run()
+            
+            _ = await task.result
+        }
+    }
+    
+    
+    func testThrottleDoesNotDropElements_whenThrottledAtSameIntervalAsElementsAreReceived_andLatestEqualTrue() async throws {
+        await withMainSerialExecutor {
+            let testClock = TestClock()
+            let stream = AsyncStream { continuation in
+                continuation.yield(1)
+                DeferredTask { () }
+                    .delay(for: .milliseconds(10))
+                    .handleEvents(receiveOutput: { _ in
+                        await testClock.advance(by: .milliseconds(10))
+                        continuation.yield(2)
+                    })
+                    .delay(for: .milliseconds(10))
+                    .handleEvents(receiveOutput: { _ in
+                        await testClock.advance(by: .milliseconds(10))
+                        continuation.yield(3)
+                    })
+                    .delay(for: .milliseconds(10))
+                    .handleEvents(receiveOutput: { _ in
+                        await testClock.advance(by: .milliseconds(10))
+                        continuation.yield(4)
+                    })
+                    .delay(for: .milliseconds(10))
+                    .handleEvents(receiveOutput: { _ in
+                        await testClock.advance(by: .milliseconds(10))
+                        continuation.yield(5)
+                        continuation.finish()
+                    })
+                    .run()
+            }.throttle(for: .milliseconds(10), clock: testClock, latest: true)
+            
+            let task = Task {
+                var elements = [Int]()
+                
+                for try await element in stream {
+                    elements.append(element)
+                }
+                
+                XCTAssertEqual(elements, [1,2,3,4,5])
+            }
+            
             await testClock.run()
             
             _ = await task.result
@@ -56,30 +121,40 @@ final class ThrottleSequenceTests: XCTestCase {
         await withMainSerialExecutor {
             let testClock = TestClock()
             let stream = AsyncStream { continuation in
-                continuation.yield("1")
+                continuation.yield(1)
                 DeferredTask { () }
-                .delay(for: .milliseconds(10))
-                .handleEvents(receiveOutput: { _ in
-                    await testClock.advance(by: .milliseconds(10))
-                    continuation.yield("2")
-                })
-                .delay(for: .milliseconds(10))
-                .handleEvents(receiveOutput: { _ in
-                    await testClock.advance(by: .milliseconds(10))
-                    continuation.yield("3")
-                    continuation.finish()
-                })
-                .run()
+                    .delay(for: .milliseconds(10))
+                    .handleEvents(receiveOutput: { _ in
+                        await testClock.advance(by: .milliseconds(10))
+                        continuation.yield(2)
+                    })
+                    .delay(for: .milliseconds(10))
+                    .handleEvents(receiveOutput: { _ in
+                        await testClock.advance(by: .milliseconds(10))
+                        continuation.yield(3)
+                    })
+                    .delay(for: .milliseconds(10))
+                    .handleEvents(receiveOutput: { _ in
+                        await testClock.advance(by: .milliseconds(10))
+                        continuation.yield(4)
+                    })
+                    .delay(for: .milliseconds(10))
+                    .handleEvents(receiveOutput: { _ in
+                        await testClock.advance(by: .milliseconds(10))
+                        continuation.yield(5)
+                        continuation.finish()
+                    })
+                    .run()
             }.throttle(for: .milliseconds(10), clock: testClock, latest: false)
             
             let task = Task {
-                var elements = [String]()
+                var elements = [Int]()
                 
                 for try await element in stream {
                     elements.append(element)
                 }
                 
-                XCTAssertEqual(elements, ["1","2","3"])
+                XCTAssertEqual(elements, [1,2,3,4,5])
             }
             
             await testClock.run()
@@ -92,30 +167,40 @@ final class ThrottleSequenceTests: XCTestCase {
         await withMainSerialExecutor {
             let testClock = TestClock()
             let stream = AsyncStream { continuation in
-                continuation.yield("1")
+                continuation.yield(1)
                 DeferredTask { () }
-                .delay(for: .milliseconds(10))
-                .handleEvents(receiveOutput: { _ in
-                    await testClock.advance(by: .milliseconds(10))
-                    continuation.yield("2")
-                })
-                .delay(for: .milliseconds(10))
-                .handleEvents(receiveOutput: { _ in
-                    await testClock.advance(by: .milliseconds(10))
-                    continuation.yield("3")
-                    continuation.finish()
-                })
-                .run()
+                    .delay(for: .milliseconds(10))
+                    .handleEvents(receiveOutput: { _ in
+                        await testClock.advance(by: .milliseconds(10))
+                        continuation.yield(2)
+                    })
+                    .delay(for: .milliseconds(10))
+                    .handleEvents(receiveOutput: { _ in
+                        await testClock.advance(by: .milliseconds(10))
+                        continuation.yield(3)
+                    })
+                    .delay(for: .milliseconds(10))
+                    .handleEvents(receiveOutput: { _ in
+                        await testClock.advance(by: .milliseconds(10))
+                        continuation.yield(4)
+                    })
+                    .delay(for: .milliseconds(10))
+                    .handleEvents(receiveOutput: { _ in
+                        await testClock.advance(by: .milliseconds(10))
+                        continuation.yield(5)
+                        continuation.finish()
+                    })
+                    .run()
             }.throttle(for: .milliseconds(20), clock: testClock, latest: true)
             
             let task = Task {
-                var elements = [String]()
+                var elements = [Int]()
                 
                 for try await element in stream {
                     elements.append(element)
                 }
                 
-                XCTAssertEqual(elements, ["1","3"])
+                XCTAssertEqual(elements, [1,2,4,5])
             }
             
             await testClock.run()
@@ -129,30 +214,40 @@ final class ThrottleSequenceTests: XCTestCase {
         await withMainSerialExecutor {
             let testClock = TestClock()
             let stream = AsyncStream { continuation in
-                continuation.yield("1")
+                continuation.yield(1)
                 DeferredTask { () }
                 .delay(for: .milliseconds(10))
                 .handleEvents(receiveOutput: { _ in
                     await testClock.advance(by: .milliseconds(10))
-                    continuation.yield("2")
+                    continuation.yield(2)
                 })
                 .delay(for: .milliseconds(10))
                 .handleEvents(receiveOutput: { _ in
                     await testClock.advance(by: .milliseconds(10))
-                    continuation.yield("3")
+                    continuation.yield(3)
+                })
+                .delay(for: .milliseconds(10))
+                .handleEvents(receiveOutput: { _ in
+                    await testClock.advance(by: .milliseconds(10))
+                    continuation.yield(4)
+                })
+                .delay(for: .milliseconds(10))
+                .handleEvents(receiveOutput: { _ in
+                    await testClock.advance(by: .milliseconds(10))
+                    continuation.yield(5)
                     continuation.finish()
                 })
                 .run()
             }.throttle(for: .milliseconds(20), clock: testClock, latest: false)
             
             let task = Task {
-                var elements = [String]()
+                var elements = [Int]()
                 
                 for try await element in stream {
                     elements.append(element)
                 }
                 
-                XCTAssertEqual(elements, ["1","2"])
+                XCTAssertEqual(elements, [1,2,3,5])
             }
             
             await testClock.run()

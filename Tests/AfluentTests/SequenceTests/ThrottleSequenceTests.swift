e41fb58be954dfe0adf390142bd14b568ca87dd0
Trip Phillips <tripp@zillowgroup.com>
2024-02-19 18:53:49 -0700

    Throttle does not throttle until first event is received

--- Tests/AfluentTests/SequenceTests/ThrottleSequenceTests.swift
+++ Tests/AfluentTests/SequenceTests/ThrottleSequenceTests.swift
@@ -14,36 +14,6 @@ import ConcurrencyExtras
 @available(iOS 16.0, *)
 final class ThrottleSequenceTests: XCTestCase {
     
-    func testThrottleDoesNotThrottleUntilFirstValueIsReceived() async throws {
-        await withMainSerialExecutor {
-            let testClock = TestClock()
-            let stream = AsyncStream { continuation in
-                DeferredTask { () }
-                    .delay(for: .milliseconds(100))
-                    .handleEvents(receiveOutput: { _ in
-                        await testClock.advance(by: .milliseconds(10))
-                        continuation.yield(1)
-                        continuation.finish()
-                    })
-                    .run()
-            }.throttle(for: .milliseconds(10), clock: testClock, latest: true)
-            
-            let task = Task {
-                var elements = [Int]()
-                
-                for try await element in stream {
-                    elements.append(element)
-                }
-                
-                XCTAssertEqual(elements, [1])
-            }
-            
-            await testClock.run()
-            
-            _ = await task.result
-        }
-    }
-    
     func testThrottleDropsAllElementsExceptLast_whenElementsAreReceivedAllAtOnce_andLatestEqualTrue() async throws {
         await withMainSerialExecutor {
             let testClock = TestClock()
@@ -110,6 +80,80 @@ final class ThrottleSequenceTests: XCTestCase {
         }
     }
     
+    func testThrottleDoesNotThrottleUntilFirstValueIsReceived() async throws {
+        await withMainSerialExecutor {
+            let testClock = TestClock()
+            let stream = AsyncStream { continuation in
+                DeferredTask { () }
+                    .delay(for: .milliseconds(100))
+                    .handleEvents(receiveOutput: { _ in
+                        await testClock.advance(by: .milliseconds(10))
+                        continuation.yield(1)
+                    })
+                    .delay(for: .milliseconds(10))
+                    .handleEvents(receiveOutput: { _ in
+                        await testClock.advance(by: .milliseconds(10))
+                        continuation.yield(2)
+                    })
+                    .delay(for: .milliseconds(10))
+                    .handleEvents(receiveOutput: { _ in
+                        await testClock.advance(by: .milliseconds(10))
+                        continuation.yield(3)
+                    })
+                    .delay(for: .milliseconds(10))
+                    .handleEvents(receiveOutput: { _ in
+                        await testClock.advance(by: .milliseconds(10))
+                        continuation.yield(4)
+                    })
+                    .delay(for: .milliseconds(10))
+                    .handleEvents(receiveOutput: { _ in
+                        await testClock.advance(by: .milliseconds(10))
+                        continuation.yield(5)
+                    })
+                    .delay(for: .milliseconds(10))
+                    .handleEvents(receiveOutput: { _ in
+                        await testClock.advance(by: .milliseconds(10))
+                        continuation.yield(6)
+                    })
+                    .delay(for: .milliseconds(10))
+                    .handleEvents(receiveOutput: { _ in
+                        await testClock.advance(by: .milliseconds(10))
+                        continuation.yield(7)
+                    })
+                    .delay(for: .milliseconds(10))
+                    .handleEvents(receiveOutput: { _ in
+                        await testClock.advance(by: .milliseconds(10))
+                        continuation.yield(8)
+                    })
+                    .delay(for: .milliseconds(10))
+                    .handleEvents(receiveOutput: { _ in
+                        await testClock.advance(by: .milliseconds(10))
+                        continuation.yield(9)
+                    })
+                    .delay(for: .milliseconds(10))
+                    .handleEvents(receiveOutput: { _ in
+                        await testClock.advance(by: .milliseconds(10))
+                        continuation.yield(10)
+                        continuation.finish()
+                    })
+                    .run()
+            }.throttle(for: .milliseconds(10), clock: testClock, latest: true)
+            
+            let task = Task {
+                var elements = [Int]()
+                
+                for try await element in stream {
+                    elements.append(element)
+                }
+                
+                XCTAssertEqual(elements, [1,2,3,4,5,6,7,8,9,10])
+            }
+            
+            await testClock.run()
+            
+            _ = await task.result
+        }
+    }
     
     func testThrottleDoesNotDropElements_whenThrottledAtSameIntervalAsElementsAreReceived_andLatestEqualTrue() async throws {
         await withMainSerialExecutor {

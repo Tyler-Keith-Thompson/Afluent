435d734dbbd11e027e93c0abb896ee29dde1af63
Trip Phillips <tripp@zillowgroup.com>
2024-02-20 13:14:31 -0700

    add tests for odd time intervals

--- Tests/AfluentTests/SequenceTests/ThrottleSequenceTests.swift
+++ Tests/AfluentTests/SequenceTests/ThrottleSequenceTests.swift
@@ -273,7 +273,7 @@ final class ThrottleSequenceTests: XCTestCase {
         }
     }
     
-    func testThrottleDropsAllElementsExceptLastElement_whenThrottledAtDifferentIntervalThanElementsAreReceived_andLatestEqualTrue() async throws {
+    func testThrottleDropsAllElementsExceptLastElement_whenThrottledAtDifferentEvenIntervalThanElementsAreReceived_andLatestEqualTrue() async throws {
         await withMainSerialExecutor {
             let testClock = TestClock()
             let stream = AsyncStream { continuation in
@@ -327,7 +327,7 @@ final class ThrottleSequenceTests: XCTestCase {
         }
     }
 
-    func testThrottleDropsAllElementsExceptFirstElement_whenThrottledAtDifferentIntervalThanElementsAreReceived_andLatestEqualFalse() async throws {
+    func testThrottleDropsAllElementsExceptFirstElement_whenThrottledAtDifferentEvenIntervalThanElementsAreReceived_andLatestEqualFalse() async throws {
         await withMainSerialExecutor {
             let testClock = TestClock()
             let stream = AsyncStream { continuation in
@@ -380,11 +380,118 @@ final class ThrottleSequenceTests: XCTestCase {
             
         }
     }
+    
+    func testThrottleDropsAllElementsExceptLastElement_whenThrottledAtDifferentOddIntervalThanElementsAreReceived_andLatestEqualTrue() async throws {
+        await withMainSerialExecutor {
+            let testClock = TestClock()
+            let stream = AsyncStream { continuation in
+                continuation.yield(1)
+                DeferredTask { () }
+                    .delayAndAdvance(clock: testClock, delay: .milliseconds(10)) {
+                        continuation.yield(2)
+                    }
+                    .delayAndAdvance(clock: testClock, delay: .milliseconds(10)) {
+                        continuation.yield(3)
+                    }
+                    .delayAndAdvance(clock: testClock, delay: .milliseconds(10)) {
+                        continuation.yield(4)
+                    }
+                    .delayAndAdvance(clock: testClock, delay: .milliseconds(10)) {
+                        continuation.yield(5)
+                    }
+                    .delayAndAdvance(clock: testClock, delay: .milliseconds(10)) {
+                        continuation.yield(6)
+                    }
+                    .delayAndAdvance(clock: testClock, delay: .milliseconds(10)) {
+                        continuation.yield(7)
+                    }
+                    .delayAndAdvance(clock: testClock, delay: .milliseconds(10)) {
+                        continuation.yield(8)
+                    }
+                    .delayAndAdvance(clock: testClock, delay: .milliseconds(10)) {
+                        continuation.yield(9)
+                    }
+                    .delayAndAdvance(clock: testClock, delay: .milliseconds(10)) {
+                        continuation.yield(10)
+                        continuation.finish()
+                    }
+                    .run()
+            }.throttle(for: .milliseconds(25), clock: testClock, latest: true)
+            
+            let task = Task {
+                var elements = [Int]()
+                
+                for try await element in stream {
+                    elements.append(element)
+                }
+                
+                XCTAssertEqual(elements, [1,3,5,7,10])
+            }
+            
+            await testClock.run()
+    
+            _ = await task.result
+            
+        }
+    }
+
+    func testThrottleDropsAllElementsExceptFirstElement_whenThrottledAtDifferentOddIntervalThanElementsAreReceived_andLatestEqualFalse() async throws {
+        await withMainSerialExecutor {
+            let testClock = TestClock()
+            let stream = AsyncStream { continuation in
+                continuation.yield(1)
+                DeferredTask { () }
+                    .delayAndAdvance(clock: testClock, delay: .milliseconds(10)) {
+                        continuation.yield(2)
+                    }
+                    .delayAndAdvance(clock: testClock, delay: .milliseconds(10)) {
+                        continuation.yield(3)
+                    }
+                    .delayAndAdvance(clock: testClock, delay: .milliseconds(10)) {
+                        continuation.yield(4)
+                    }
+                    .delayAndAdvance(clock: testClock, delay: .milliseconds(10)) {
+                        continuation.yield(5)
+                    }
+                    .delayAndAdvance(clock: testClock, delay: .milliseconds(10)) {
+                        continuation.yield(6)
+                    }
+                    .delayAndAdvance(clock: testClock, delay: .milliseconds(10)) {
+                        continuation.yield(7)
+                    }
+                    .delayAndAdvance(clock: testClock, delay: .milliseconds(10)) {
+                        continuation.yield(8)
+                    }
+                    .delayAndAdvance(clock: testClock, delay: .milliseconds(10)) {
+                        continuation.yield(9)
+                    }
+                    .delayAndAdvance(clock: testClock, delay: .milliseconds(10)) {
+                        continuation.yield(10)
+                        continuation.finish()
+                    }
+                    .run()
+            }.throttle(for: .milliseconds(25), clock: testClock, latest: false)
+            
+            let task = Task {
+                var elements = [Int]()
+                
+                for try await element in stream {
+                    elements.append(element)
+                }
+                
+                XCTAssertEqual(elements, [1,2,4,6,9])
+            }
+            
+            await testClock.run()
+    
+            _ = await task.result
+            
+        }
+    }
 }
 
 @available(iOS 16.0, *)
 private extension AsynchronousUnitOfWork {
-    
     func delayAndAdvance<D: DurationProtocol & Hashable>(clock: TestClock<D>, delay: Duration, completion: @escaping () -> Void) -> any AsynchronousUnitOfWork {
         self.delay(for: delay)
             .handleEvents(receiveOutput: { _ in
@@ -395,5 +502,4 @@ private extension AsynchronousUnitOfWork {
                 completion()
             })
     }
-    
 }

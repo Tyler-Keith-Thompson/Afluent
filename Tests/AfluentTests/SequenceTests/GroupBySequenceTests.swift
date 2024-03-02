7c455bed1da338de69b6c8245d87beaa73ad6f63
Trip Phillips <tripp@zillowgroup.com>
2024-03-01 18:18:58 -0700

    GroupBy returns keys with and without transformations

--- Tests/AfluentTests/SequenceTests/GroupBySequenceTests.swift
+++ Tests/AfluentTests/SequenceTests/GroupBySequenceTests.swift
@@ -12,7 +12,7 @@ import Clocks
 import ConcurrencyExtras
 
 final class GroupBySequenceTests: XCTestCase {
-    func testGroupByWithEmptySequenceReturnsEmpty() async throws {
+    func testGroupByWithEmptySequenceReturnsEmptyKeys() async throws {
         await withMainSerialExecutor {
             let stream = AsyncStream<String> { continuation in
                 continuation.finish()
@@ -28,6 +28,54 @@ final class GroupBySequenceTests: XCTestCase {
             let task = Task {
                 let keys = try await stream.first()
                 XCTAssert(keys?.isEmpty ?? true)
+            }
+            
+            _ = await task.result
+        }
+    }
+    
+    func testGroupByWithEmptySequenceReturnsKeysWithoutTransformation() async throws {
+        await withMainSerialExecutor {
+            let stream = AsyncStream<String> { continuation in
+                continuation.yield("a")
+                continuation.yield("b")
+                continuation.yield("c")
+                continuation.finish()
+            }
+                .groupBy { $0 }
+                .map {
+                    $0.key
+                }
+                .collect()
+            
+            let task = Task {
+                let keys = try await stream.first()
+                XCTAssertEqual(keys, ["a", "b", "c"])
+            }
+            
+            _ = await task.result
+        }
+    }
+    
+    func testGroupByWithEmptySequenceReturnsKeysWithTransformation() async throws {
+        await withMainSerialExecutor {
+            let stream = AsyncStream<String> { continuation in
+                continuation.yield("a")
+                continuation.yield("b")
+                continuation.yield("c")
+                continuation.finish()
+            }
+                .groupBy { element in
+                    return element.uppercased()
+                }
+                .map {
+                    $0.key
+                }
+                .collect()
+            
+            let task = Task {
+                let keys = try await stream.first()
+                XCTAssertEqual(keys, ["A", "B", "C"])
             }
             
             _ = await task.result

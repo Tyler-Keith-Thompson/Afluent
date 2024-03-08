f6150f89aca3b50df26f2e6f0c19d1f6985c2521
Trip Phillips <tripp@zillowgroup.com>
2024-03-08 09:44:04 -0600

    WIP: progress but tests don't pass yet

--- Sources/Afluent/SequenceOperators/GroupBySequence.swift
+++ Sources/Afluent/SequenceOperators/GroupBySequence.swift
@@ -9,29 +9,76 @@ import Foundation
 
 extension AsyncSequences {
     public struct GroupBy<Upstream: AsyncSequence, Key: Hashable>: AsyncSequence {
-        public typealias Element = GroupedSequence<Key, Upstream>
+        public typealias Element = KeyedAsyncSequence<Key, Upstream>
         let upstream: Upstream
         let keySelector: (Upstream.Element) async -> Key
-
+        
+        class KeyedSequences {
+            var sequences: [Key: Element] {
+                get {
+                    lock.protect {
+                        _sequences
+                    }
+                }
+                set {
+                    lock.protect {
+                        _sequences = newValue
+                    }
+                }
+            }
+            
+            private var _sequences: [Key: Element]
+            
+            private let lock = NSRecursiveLock()
+            
+            init(sequences: [Key : Element] = [:]) {
+                self._sequences = sequences
+            }
+        }
+        
         init(upstream: Upstream, keySelector: @escaping (Upstream.Element) async -> Key) {
             self.upstream = upstream
             self.keySelector = keySelector
         }
         
         public struct AsyncIterator: AsyncIteratorProtocol {
-            public typealias Element = GroupedSequence<Key, Upstream>
-            let upstream: Upstream
+            public typealias Element = KeyedAsyncSequence<Key, Upstream>
+            var upstream: Upstream
             let keySelector: (Upstream.Element) async -> Key
             
-            lazy var iterator = upstream.makeAsyncIterator()
+            var keyedSequences = KeyedSequences()
+            var iterator: AsyncThrowingStream<Element, Error>.Iterator?
             
-            public mutating func next() async throws -> AsyncIterator.Element? {
+            public mutating func next() async throws -> Element? {
                 try Task.checkCancellation()
-                guard let element = try await iterator.next() else {
-                    return nil
+                if iterator == nil {
+                    iterator = AsyncThrowingStream<Element, Error> { continuation in
+                        
+                        let upstream = self.upstream
+                        let keySelector = self.keySelector
+                        let keyedSequences = self.keyedSequences
+                        
+                        Task {
+                            for try await el in upstream {
+                                let key = await keySelector(el)
+                                if keyedSequences.sequences[key] != nil {
+                                    keyedSequences.sequences[key]?.elements.append(el)
+                                } else {
+                                    let keyedAsyncSequence = KeyedAsyncSequence(upstream: upstream, elements: [el], key: key)
+                                    keyedSequences.sequences[key] = keyedAsyncSequence
+                                    continuation.yield(keyedAsyncSequence)
+                                }
+                            }
+                            continuation.finish()
+                        }
+                    }.makeAsyncIterator()
                 }
-                let key = await keySelector(element)
-                return GroupedSequence(upstream: upstream, key: key)
+                
+                while let element = try await iterator?.next() {
+                    return element
+                }
+                
+                return nil
             }
         }
         
@@ -40,30 +87,36 @@ extension AsyncSequences {
         }
     }
     
-    public struct GroupedSequence<Key: Hashable, Upstream: AsyncSequence>: AsyncSequence {
-        
+    public struct KeyedAsyncSequence<Key: Hashable, Upstream: AsyncSequence>: AsyncSequence {
         public typealias Element = Upstream.Element
-        let upstream: Upstream
-        public let key: Key
+        var upstream: Upstream
+        var elements: [Element]
+        let key: Key
 
-        init(upstream: Upstream, key: Key) {
+        init(upstream: Upstream, elements: [Element], key: Key) {
             self.upstream = upstream
+            self.elements = elements
             self.key = key
         }
         
         public struct AsyncIterator: AsyncIteratorProtocol {
-            let upstream: Upstream
             
-            lazy var iterator = upstream.makeAsyncIterator()
+            var elements: [Element]
+            var iterator: Array<Element>.Iterator?
             
             public mutating func next() async throws -> Element? {
                 try Task.checkCancellation()
-                return try await iterator.next()
+                
+                if iterator == nil {
+                    iterator = elements.makeIterator()
+                }
+                
+                return iterator?.next()
             }
         }
         
         public func makeAsyncIterator() -> AsyncIterator {
-            AsyncIterator(upstream: upstream)
+            AsyncIterator(elements: elements)
         }
     }
 }
--- Tests/AfluentTests/SequenceTests/GroupBySequenceTests.swift
+++ Tests/AfluentTests/SequenceTests/GroupBySequenceTests.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 import XCTest
-import Afluent
+@testable import Afluent
 import Clocks
 import ConcurrencyExtras
 
@@ -34,7 +34,7 @@ final class GroupBySequenceTests: XCTestCase {
         }
     }
     
-    func testGroupByWithEmptySequenceReturnsKeysWithoutTransformation() async throws {
+    func testGroupByWithPopulatedSequenceReturnsKeysWithoutTransformation() async throws {
         await withMainSerialExecutor {
             let stream = AsyncStream<String> { continuation in
                 continuation.yield("a")
@@ -57,7 +57,7 @@ final class GroupBySequenceTests: XCTestCase {
         }
     }
     
-    func testGroupByWithEmptySequenceReturnsKeysWithTransformation() async throws {
+    func testGroupByWithPopulatedSequenceReturnsKeysWithTransformation() async throws {
         await withMainSerialExecutor {
             let stream = AsyncStream<String> { continuation in
                 continuation.yield("a")
@@ -76,6 +76,66 @@ final class GroupBySequenceTests: XCTestCase {
             let task = Task {
                 let keys = try await stream.first()
                 XCTAssertEqual(keys, ["A", "B", "C"])
+            }
+            
+            _ = await task.result
+        }
+    }
+    
+    func testGroupByWithPopulatedSequenceGroupsByKeys() async throws {
+        await withMainSerialExecutor {
+            let stream = AsyncStream<String> { continuation in
+                continuation.yield("a")
+                continuation.yield("b")
+                continuation.yield("c")
+                continuation.yield("c")
+                continuation.finish()
+            }
+                .groupBy { element in
+                    return element
+                }
+                .map {
+                    $0.key
+                }
+                .collect()
+            
+            let task = Task {
+                let keys = try await stream.first()
+                XCTAssertEqual(keys, ["a", "b", "c"])
+            }
+            
+            _ = await task.result
+        }
+    }
+    
+    func testGroupByWithPopulatedSequenceGroupsByKeysWithSequences() async throws {
+        await withMainSerialExecutor {
+            let stream = AsyncStream<String> { continuation in
+                continuation.yield("a")
+                continuation.yield("b")
+                continuation.yield("c")
+                continuation.yield("c")
+                continuation.finish()
+            }
+                .groupBy { element in
+                    return element
+                }
+            
+            let task = Task {
+                var results = [String: AsyncSequences.KeyedAsyncSequence<String, AsyncStream<String>>]()
+                
+                for try await sequence in stream {
+                    results[sequence.key] = sequence
+                }
+                
+                let a = try await results["a"]?.collect().first()
+                XCTAssertEqual(a, ["a"])
+                
+                let b = try await results["b"]?.collect().first()
+                XCTAssertEqual(b, ["b"])
+                
+                let c = try await results["c"]?.collect().first()
+                XCTAssertEqual(c, ["c", "c"])
             }
             
             _ = await task.result

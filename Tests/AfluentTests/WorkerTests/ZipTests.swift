6a7c63b33bd4261bd3f7e658acf8f5084f7ea44b
Tyler Thompson <Tyler.Keith.Thompson@gmail.com>
2024-04-17 15:56:00 -0600

    [skip ci] swift-testing - Zip tests - TT

--- Tests/AfluentTests/WorkerTests/ZipTests.swift
+++ Tests/AfluentTests/WorkerTests/ZipTests.swift
@@ -7,92 +7,92 @@
 
 import Afluent
 import Foundation
-import XCTest
+import Testing
 
-final class ZipTests: XCTestCase {
-    func testZipCombinesTasks_WithExplicitFailure() async throws {
+struct ZipTests {
+    @Test func zipCombinesTasks_WithExplicitFailure() async throws {
         let t1 = DeferredTask<Int> { 1 }
 
         let t2 = DeferredTask<String> { "A" }
         let t3 = t2.zip(t1)
         let val = try await t3.execute() // Steak sauce!!!
-        XCTAssertEqual(val.0, "A")
-        XCTAssertEqual(val.1, 1)
+        #expect(val.0 == "A")
+        #expect(val.1 == 1)
     }
 
-    func testZipCombinesTasks() async throws {
+    @Test func zipCombinesTasks() async throws {
         let t1 = DeferredTask { 1 }
 
         let t2 = DeferredTask { "A" }
         let t3 = t2.zip(t1)
         let val = try await t3.execute() // Steak sauce!!!
-        XCTAssertEqual(val.0, "A")
-        XCTAssertEqual(val.1, 1)
+        #expect(val.0 == "A")
+        #expect(val.1 == 1)
     }
 
-    func testZipTransformCombinesTasks_WithExplicitFailure() async throws {
+    @Test func zipTransformCombinesTasks_WithExplicitFailure() async throws {
         let t1 = DeferredTask<Int> { 1 }
 
         let t2 = DeferredTask<String> { "A" }
         let t3 = t2.zip(t1) { $0 + String(describing: $1) }
         let val = try await t3.execute() // Steak sauce!!!
-        XCTAssertEqual(val, "A1")
+        #expect(val == "A1")
     }
 
-    func testZipTransformCombinesTasks() async throws {
+    @Test func zipTransformCombinesTasks() async throws {
         let t1 = DeferredTask { 1 }
 
         let t2 = DeferredTask { "A" }
         let t3 = t2.zip(t1) { $0 + String(describing: $1) }
         let val = try await t3.execute() // Steak sauce!!!
-        XCTAssertEqual(val, "A1")
+        #expect(val == "A1")
     }
 
-    func testZip3CombinesTasks_WithExplicitFailure() async throws {
+    @Test func zip3CombinesTasks_WithExplicitFailure() async throws {
         let t1 = DeferredTask<Int> { 1 }
         let t2 = DeferredTask<String> { "A" }
         let t3 = DeferredTask<Bool> { true }
 
         let t4 = t2.zip(t1, t3)
         let val = try await t4.execute() // Steak sauce!!!
-        XCTAssertEqual(val.0, "A")
-        XCTAssertEqual(val.1, 1)
-        XCTAssertEqual(val.2, true)
+        #expect(val.0 == "A")
+        #expect(val.1 == 1)
+        #expect(val.2 == true)
     }
 
-    func testZip3CombinesTasks() async throws {
+    @Test func zip3CombinesTasks() async throws {
         let t1 = DeferredTask { 1 }
         let t2 = DeferredTask { "A" }
         let t3 = DeferredTask { true }
 
         let t4 = t2.zip(t1, t3)
         let val = try await t4.execute() // Steak sauce!!!
-        XCTAssertEqual(val.0, "A")
-        XCTAssertEqual(val.1, 1)
-        XCTAssertEqual(val.2, true)
+        #expect(val.0 == "A")
+        #expect(val.1 == 1)
+        #expect(val.2 == true)
     }
 
-    func testZip3TransformCombinesTasks_WithExplicitFailure() async throws {
+    @Test func zip3TransformCombinesTasks_WithExplicitFailure() async throws {
         let t1 = DeferredTask<Int> { 1 }
         let t2 = DeferredTask<String> { "A" }
         let t3 = DeferredTask<Bool> { true }
 
         let t4 = t2.zip(t1, t3) { $0 + String(describing: $1) + String(describing: $2) }
         let val = try await t4.execute() // Steak sauce!!!
-        XCTAssertEqual(val, "A1true")
+        #expect(val == "A1true")
     }
 
-    func testZip3TransformCombinesTasks() async throws {
+    @Test func zip3TransformCombinesTasks() async throws {
         let t1 = DeferredTask { 1 }
         let t2 = DeferredTask { "A" }
         let t3 = DeferredTask { true }
 
         let t4 = t2.zip(t1, t3) { $0 + String(describing: $1) + String(describing: $2) }
         let val = try await t4.execute() // Steak sauce!!!
-        XCTAssertEqual(val, "A1true")
+        #expect(val == "A1true")
     }
 
-    func testZip4CombinesTasks_WithExplicitFailure() async throws {
+    @Test func zip4CombinesTasks_WithExplicitFailure() async throws {
         let t1 = DeferredTask<Int> { 1 }
         let t2 = DeferredTask<String> { "A" }
         let t3 = DeferredTask<Bool> { true }
@@ -100,13 +100,13 @@ final class ZipTests: XCTestCase {
 
         let t5 = t2.zip(t1, t3, t4)
         let val = try await t5.execute() // Steak sauce!!!
-        XCTAssertEqual(val.0, "A")
-        XCTAssertEqual(val.1, 1)
-        XCTAssertEqual(val.2, true)
-        XCTAssertEqual(val.3, Character("!"))
+        #expect(val.0 == "A")
+        #expect(val.1 == 1)
+        #expect(val.2 == true)
+        #expect(val.3 == Character("!"))
     }
 
-    func testZip4CombinesTasks() async throws {
+    @Test func zip4CombinesTasks() async throws {
         let t1 = DeferredTask { 1 }
         let t2 = DeferredTask { "A" }
         let t3 = DeferredTask { true }
@@ -114,13 +114,13 @@ final class ZipTests: XCTestCase {
 
         let t5 = t2.zip(t1, t3, t4)
         let val = try await t5.execute() // Steak sauce!!!
-        XCTAssertEqual(val.0, "A")
-        XCTAssertEqual(val.1, 1)
-        XCTAssertEqual(val.2, true)
-        XCTAssertEqual(val.3, Character("!"))
+        #expect(val.0 == "A")
+        #expect(val.1 == 1)
+        #expect(val.2 == true)
+        #expect(val.3 == Character("!"))
     }
 
-    func testZip4TransformCombinesTasks_WithExplicitFailure() async throws {
+    @Test func zip4TransformCombinesTasks_WithExplicitFailure() async throws {
         let t1 = DeferredTask<Int> { 1 }
         let t2 = DeferredTask<String> { "A" }
         let t3 = DeferredTask<Bool> { true }
@@ -128,10 +128,10 @@ final class ZipTests: XCTestCase {
 
         let t5 = t2.zip(t1, t3, t4) { $0 + String(describing: $1) + String(describing: $2) + String(describing: $3) }
         let val = try await t5.execute() // Steak sauce!!!
-        XCTAssertEqual(val, "A1true!")
+        #expect(val == "A1true!")
     }
 
-    func testZip4TransformCombinesTasks() async throws {
+    @Test func zip4TransformCombinesTasks() async throws {
         let t1 = DeferredTask { 1 }
         let t2 = DeferredTask { "A" }
         let t3 = DeferredTask { true }
@@ -139,6 +139,6 @@ final class ZipTests: XCTestCase {
 
         let t5 = t2.zip(t1, t3, t4) { $0 + String(describing: $1) + String(describing: $2) + String(describing: $3) }
         let val = try await t5.execute() // Steak sauce!!!
-        XCTAssertEqual(val, "A1true!")
+        #expect(val == "A1true!")
     }
 }

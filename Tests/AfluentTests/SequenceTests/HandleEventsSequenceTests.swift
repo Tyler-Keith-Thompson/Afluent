d293af7d7a343d9ce9ae03241946ead6155dca3c
Tyler Thompson <Tyler.Keith.Thompson@gmail.com>
2024-06-15 17:26:55 -0600

    [xcode-16] - HandleEventsSequenceTests completely back - TT

--- Tests/AfluentTests/SequenceTests/HandleEventsSequenceTests.swift
+++ Tests/AfluentTests/SequenceTests/HandleEventsSequenceTests.swift
@@ -6,91 +6,81 @@
 //
 
 import Afluent
+import Atomics
 import ConcurrencyExtras
 import Foundation
 import Testing
 
 struct HandleEventsSequenceTests {
-    #warning("Revisit")
-//    @available(macOS 13.0, iOS 16.0, watchOS 9.0, tvOS 16.0, visionOS 1.0, *)
-//    @Test func handleMakeIterator() async throws {
-//        actor Test {
-//            var iteratorMade = false
-//
-//            func makeIterator() { iteratorMade = true }
-//        }
-//        let test = Test()
-//
-//        _ = await Task {
-//            var subtask: Task<Void, Never>?
-//            _ = DeferredTask { }
-//                .toAsyncSequence()
-//                .handleEvents(receiveMakeIterator: {
-//                    subtask = Task {
-//                        await test.makeIterator()
-//                    }
-//                })
-//                .makeAsyncIterator()
-//            _ = await subtask?.result
-//        }.value
-//
-//        let iteratorMade = await test.iteratorMade
-//
-//        #expect(iteratorMade)
-//    }
-//
-//    @available(macOS 13.0, iOS 16.0, watchOS 9.0, tvOS 16.0, visionOS 1.0, *)
-//    @Test func handleNext() async throws {
-//        actor Test {
-//            var nextCalled: Int = 0
-//
-//            func next() { nextCalled += 1 }
-//        }
-//        let test = Test()
-//
-//        let values = Array(0 ... 9)
-//
-//        let task = Task {
-//            let sequence = values.async.handleEvents(receiveNext: {
-//                await test.next()
-//            })
-//
-//            for try await _ in sequence { }
-//        }
-//
-//        try await task.value
-//
-//        let nextCalled = await test.nextCalled
-//
-//        #expect(nextCalled == values.count + 1) // values + finish
-//    }
-//
-//    @available(macOS 13.0, iOS 16.0, watchOS 9.0, tvOS 16.0, visionOS 1.0, *)
-//    @Test func handleOutput() async throws {
-//        actor Test {
-//            var output: Any?
-//
-//            func output(_ any: Any?) { output = any }
-//        }
-//        let test = Test()
-//
-//        let task = Task {
-//            try await DeferredTask {
-//                1
-//            }
-//            .toAsyncSequence()
-//            .handleEvents(receiveOutput: {
-//                await test.output($0)
-//            })
-//            .first()
-//        }
-//
-//        _ = try await task.value
-//
-//        let output = await test.output
-//
-//        #expect(output as? Int == 1)
-//    }
+    @available(macOS 13.0, iOS 16.0, watchOS 9.0, tvOS 16.0, visionOS 1.0, *)
+    @Test func handleMakeIterator() async throws {
+        let iteratorMade = ManagedAtomic(false)
+
+        _ = await Task {
+            _ = DeferredTask { }
+                .toAsyncSequence()
+                .handleEvents(receiveMakeIterator: {
+                    iteratorMade.store(true, ordering: .sequentiallyConsistent)
+                })
+                .makeAsyncIterator()
+        }.value
+
+        let actual = iteratorMade.load(ordering: .sequentiallyConsistent)
+        #expect(actual)
+    }
+
+    @available(macOS 13.0, iOS 16.0, watchOS 9.0, tvOS 16.0, visionOS 1.0, *)
+    @Test func handleNext() async throws {
+        actor Test {
+            var nextCalled: Int = 0
+
+            func next() { nextCalled += 1 }
+        }
+        let test = Test()
+
+        let values = Array(0 ... 9)
+
+        let task = Task {
+            let sequence = values.async.handleEvents(receiveNext: {
+                await test.next()
+            })
+
+            for try await _ in sequence { }
+        }
+
+        try await task.value
+
+        let nextCalled = await test.nextCalled
+
+        #expect(nextCalled == values.count + 1) // values + finish
+    }
+
+    @available(macOS 13.0, iOS 16.0, watchOS 9.0, tvOS 16.0, visionOS 1.0, *)
+    @Test func handleOutput() async throws {
+        actor Test {
+            var output: Int?
+
+            func output(_ any: Int?) { output = any }
+        }
+        let test = Test()
+
+        let task = Task {
+            try await DeferredTask {
+                1
+            }
+            .toAsyncSequence()
+            .handleEvents(receiveOutput: {
+                await test.output($0)
+            })
+            .first()
+        }
+
+        _ = try await task.value
+
+        let output = await test.output
+
+        #expect(output == 1)
+    }
 
     @available(macOS 13.0, iOS 16.0, watchOS 9.0, tvOS 16.0, visionOS 1.0, *)
     @Test func handleComplete() async throws {

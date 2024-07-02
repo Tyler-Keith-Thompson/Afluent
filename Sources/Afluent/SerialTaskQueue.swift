4b75bf502648a448a5d2dcf9861536c2073ce81d
Tyler Thompson <33705774+Tyler-Keith-Thompson@users.noreply.github.com>
2024-07-02 17:21:50 -0600

    [SerialTaskQueue] - Added SerialTaskQueue - TT (#115)

---
+++ Sources/Afluent/SerialTaskQueue.swift
@@ -0,0 +1,59 @@
+//
+//  SerialTaskQueue.swift
+//
+//
+//  Created by Tyler Thompson on 6/30/24.
+//
+
+import Atomics
+import Foundation
+
+/// A serial task queue that ensures tasks are executed one at a time.
+///
+/// This actor manages a queue of tasks, ensuring that only one task is executed at a time. If a task is currently running, any new tasks are added to the queue and executed sequentially.
+///
+/// ## Discussion
+///
+/// The `SerialTaskQueue` actor provides a way to manage task execution in a serial manner. When a new task is queued, it checks if another task is currently running. If no task is running, the new task is executed immediately. If a task is already running, the new task is added to a queue and will be executed once the current task completes.
+/// This actor is useful in scenarios where tasks must be executed in a specific order or when you need to ensure that only one task is executed at a time to avoid race conditions.
+public final class SerialTaskQueue<T: Sendable>: @unchecked Sendable {
+    private var subscribers = Set<AnyCancellable>()
+    private var (stream, deferredTaskContinuation) = AsyncStream<AnyAsynchronousUnitOfWork<Void>>.makeStream()
+
+    public init() {
+        DeferredTask {
+            for await task in stream {
+                try await task.execute()
+            }
+        }
+        .subscribe()
+        .store(in: &subscribers)
+    }
+
+    /// Queues a task to be executed serially.
+    ///
+    /// If no task is currently running, the provided task is executed immediately.
+    /// If a task is running, the provided task is added to the queue and will be executed once the current task completes.
+    ///
+    /// - Parameter task: The asynchronous task to be queued and executed.
+    /// - Returns: The result of the task.
+    /// - Throws: An error if the task throws an error.
+    public func queue(_ task: @Sendable @escaping () async throws -> T) async throws -> T {
+        return try await withUnsafeThrowingContinuation { [weak self] continuation in
+            guard let self else { continuation.resume(throwing: CancellationError()); return }
+            self.deferredTaskContinuation.yield(
+                DeferredTask {
+                    try continuation.resume(returning: await task())
+                }.handleEvents(receiveCancel: {
+                    continuation.resume(throwing: CancellationError())
+                })
+                .eraseToAnyUnitOfWork()
+            )
+        }
+    }
+
+    /// Cancels all the ongoing tasks in the queue
+    public func cancelAll() {
+        subscribers.removeAll()
+    }
+}
---
+++ Tests/AfluentTests/SerialTaskQueueTests.swift
@@ -0,0 +1,87 @@
+//
+//  SerialTaskQueueTests.swift
+//
+//
+//  Created by Tyler Thompson on 6/30/24.
+//
+import Atomics
+import ConcurrencyExtras
+import Testing
+
+import Afluent
+
+struct SerialTaskQueueTests {
+    @Test func serialTaskQueueSchedulesOneTaskAtATime_EvenWhenSchedulingIsConcurrent() async throws {
+        let queue = SerialTaskQueue<Int>()
+        actor Test {
+            var isExecuting = false
+
+            func store(_ val: Bool) {
+                isExecuting = val
+            }
+        }
+        let test = Test()
+        let results = try await withThrowingTaskGroup(of: Int.self, returning: [Int].self) { group in
+            for i in 1 ... 100 {
+                group.addTask {
+                    try await queue.queue {
+                        let executing = await test.isExecuting
+                        #expect(executing == false)
+                        await test.store(true)
+                        try await Task.sleep(nanoseconds: 1)
+                        await test.store(false)
+                        return i
+                    }
+                }
+            }
+
+            var results = [Int]()
+            for try await result in group {
+                results.append(result)
+            }
+            return results
+        }
+        // Why sort the results? Because task groups don't make any guarantees, just because I asked it to schedule these in order doesn't mean it did.
+        #expect(results.sorted() == Array(1 ... 100))
+    }
+
+    @Test func queueCanCancelOngoingTasks() async throws {
+        try await withMainSerialExecutor {
+            let sub = SingleValueSubject<Void>()
+            let queue = SerialTaskQueue<Void>()
+            let executed = ManagedAtomic(false)
+            async let _: Void = try await queue.queue {
+                try sub.send()
+                try Task.checkCancellation()
+                try await Task.sleep(for: .milliseconds(20))
+                executed.store(true, ordering: .sequentiallyConsistent)
+            }
+            await Task.yield()
+            try await sub.execute()
+            queue.cancelAll()
+            let actual = executed.load(ordering: .sequentiallyConsistent)
+            #expect(actual == false)
+        }
+    }
+
+    #if swift(>=6.0)
+        @Test func queueCanCancelOngoingTasks_OnDeinit() async throws {
+            try await withMainSerialExecutor {
+                let sub = SingleValueSubject<Void>()
+                var queue: SerialTaskQueue<Void>? = SerialTaskQueue<Void>()
+                let executed = ManagedAtomic(false)
+                async let _: Void = try await #require(queue).queue {
+                    try sub.send()
+                    try Task.checkCancellation()
+                    try await Task.sleep(for: .milliseconds(20))
+                    executed.store(true, ordering: .sequentiallyConsistent)
+                }
+                await Task.yield()
+                try await sub.execute()
+                queue = nil
+                let actual = executed.load(ordering: .sequentiallyConsistent)
+                #expect(actual == false)
+            }
+        }
+    #endif
+}

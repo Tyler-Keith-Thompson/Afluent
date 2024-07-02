c77a1b5d711d0c41e66c43c98f2ceaf94615a047
Tyler Thompson <Tyler.Keith.Thompson@gmail.com>
2024-07-02 17:05:20 -0600

    [SerialTaskQueue] - Rethink serial queue - TT

--- Sources/Afluent/SerialTaskQueue.swift
+++ Sources/Afluent/SerialTaskQueue.swift
@@ -17,10 +17,18 @@ import Foundation
 /// The `SerialTaskQueue` actor provides a way to manage task execution in a serial manner. When a new task is queued, it checks if another task is currently running. If no task is running, the new task is executed immediately. If a task is already running, the new task is added to a queue and will be executed once the current task completes.
 /// This actor is useful in scenarios where tasks must be executed in a specific order or when you need to ensure that only one task is executed at a time to avoid race conditions.
 public final class SerialTaskQueue<T: Sendable>: @unchecked Sendable {
-    private var lock = NSRecursiveLock()
-    private var tasks = [DeferredTask<Void>]()
+    private var subscribers = Set<AnyCancellable>()
+    private var (stream, deferredTaskContinuation) = AsyncStream<AnyAsynchronousUnitOfWork<Void>>.makeStream()
 
-    public init() { }
+    public init() {
+        DeferredTask {
+            for await task in stream {
+                try await task.execute()
+            }
+        }
+        .subscribe()
+        .store(in: &subscribers)
+    }
 
     /// Queues a task to be executed serially.
     ///
@@ -31,44 +39,21 @@ public final class SerialTaskQueue<T: Sendable>: @unchecked Sendable {
     /// - Returns: The result of the task.
     /// - Throws: An error if the task throws an error.
     public func queue(_ task: @Sendable @escaping () async throws -> T) async throws -> T {
-        if lock.protect({ tasks.isEmpty }) {
-            return try await task()
-        }
-        return try await withUnsafeThrowingContinuation { continuation in
-            append(DeferredTask { [weak self] in
-                guard let self else {
-                    continuation.resume(throwing: CancellationError())
-                    return
-                }
-                do {
+        return try await withUnsafeThrowingContinuation { [weak self] continuation in
+            guard let self else { continuation.resume(throwing: CancellationError()); return }
+            self.deferredTaskContinuation.yield(
+                DeferredTask {
                     try continuation.resume(returning: await task())
-                } catch {
-                    continuation.resume(throwing: error)
-                }
-                self.popAndExecute()
-            })
+                }.handleEvents(receiveCancel: {
+                    continuation.resume(throwing: CancellationError())
+                })
+                .eraseToAnyUnitOfWork()
+            )
         }
     }
 
     /// Cancels all the ongoing tasks in the queue
     public func cancelAll() {
-        lock.protect { tasks }.forEach { $0.cancel() }
-    }
-
-    private func append(_ task: DeferredTask<Void>) {
-        lock.protect {
-            tasks.append(task)
-        }
-    }
-
-    private func popAndExecute() {
-        lock.protect { () -> DeferredTask<Void>? in
-            guard !tasks.isEmpty else { return nil }
-            return tasks.removeFirst()
-        }?.run()
-    }
-
-    deinit {
-        tasks.forEach { $0.cancel() }
+        subscribers.removeAll()
     }
 }
--- Tests/AfluentTests/SerialTaskQueueTests.swift
+++ Tests/AfluentTests/SerialTaskQueueTests.swift
@@ -12,39 +12,37 @@ import Afluent
 
 struct SerialTaskQueueTests {
     @Test func serialTaskQueueSchedulesOneTaskAtATime_EvenWhenSchedulingIsConcurrent() async throws {
-        await withKnownIssue(isIntermittent: true) {
-            let queue = SerialTaskQueue<Int>()
-            actor Test {
-                var isExecuting = false
+        let queue = SerialTaskQueue<Int>()
+        actor Test {
+            var isExecuting = false
 
-                func store(_ val: Bool) {
-                    isExecuting = val
-                }
+            func store(_ val: Bool) {
+                isExecuting = val
             }
-            let test = Test()
-            let results = try await withThrowingTaskGroup(of: Int.self, returning: [Int].self) { group in
-                for i in 1 ... 100 {
-                    group.addTask {
-                        try await queue.queue {
-                            let executing = await test.isExecuting
-                            #expect(executing == false)
-                            await test.store(true)
-                            try await Task.sleep(nanoseconds: 1)
-                            await test.store(false)
-                            return i
-                        }
+        }
+        let test = Test()
+        let results = try await withThrowingTaskGroup(of: Int.self, returning: [Int].self) { group in
+            for i in 1 ... 100 {
+                group.addTask {
+                    try await queue.queue {
+                        let executing = await test.isExecuting
+                        #expect(executing == false)
+                        await test.store(true)
+                        try await Task.sleep(nanoseconds: 1)
+                        await test.store(false)
+                        return i
                     }
                 }
-
-                var results = [Int]()
-                for try await result in group {
-                    results.append(result)
-                }
-                return results
             }
-            // Why sort the results? Because task groups don't make any guarantees, just because I asked it to schedule these in order doesn't mean it did.
-            #expect(results.sorted() == Array(1 ... 100))
+
+            var results = [Int]()
+            for try await result in group {
+                results.append(result)
+            }
+            return results
         }
+        // Why sort the results? Because task groups don't make any guarantees, just because I asked it to schedule these in order doesn't mean it did.
+        #expect(results.sorted() == Array(1 ... 100))
     }
 
     @Test func queueCanCancelOngoingTasks() async throws {
@@ -55,12 +53,31 @@ struct SerialTaskQueueTests {
             async let _: Void = try await queue.queue {
                 try sub.send()
                 try Task.checkCancellation()
-                try await Task.sleep(for: .seconds(20))
+                try await Task.sleep(for: .milliseconds(20))
                 executed.store(true, ordering: .sequentiallyConsistent)
             }
             await Task.yield()
             try await sub.execute()
             queue.cancelAll()
+            let actual = executed.load(ordering: .sequentiallyConsistent)
+            #expect(actual == false)
+        }
+    }
+
+    @Test func queueCanCancelOngoingTasks_OnDeinit() async throws {
+        try await withMainSerialExecutor {
+            let sub = SingleValueSubject<Void>()
+            var queue: SerialTaskQueue<Void>? = SerialTaskQueue<Void>()
+            let executed = ManagedAtomic(false)
+            async let _: Void = try await #require(queue).queue {
+                try sub.send()
+                try Task.checkCancellation()
+                try await Task.sleep(for: .milliseconds(20))
+                executed.store(true, ordering: .sequentiallyConsistent)
+            }
+            await Task.yield()
+            try await sub.execute()
+            queue = nil
             let actual = executed.load(ordering: .sequentiallyConsistent)
             #expect(actual == false)
         }

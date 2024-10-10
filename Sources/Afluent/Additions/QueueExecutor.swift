da953948442b5a34e9f7a07a6f4c525f44517fb7
Annalise Mariottini <annalise.mariottini@columbia.edu>
2024-10-10 18:16:04 -0400

    add shared instances for QueueExecutor, add test for task locals

--- Sources/Afluent/Additions/QueueExecutor.swift
+++ Sources/Afluent/Additions/QueueExecutor.swift
@@ -26,16 +26,39 @@ public final class QueueExecutor: TaskExecutor, Sendable, CustomStringConvertibl
     public var description: String {
         "\(Self.self)\(ObjectIdentifier(self))"
     }
+
+    fileprivate static let main = QueueExecutor(queue: .main)
+    fileprivate static let globalBackground = QueueExecutor(queue: .global(qos: .background))
+    fileprivate static let globalUtility = QueueExecutor(queue: .global(qos: .utility))
+    fileprivate static let globalDefault = QueueExecutor(queue: .global(qos: .default))
+    fileprivate static let globalUserInitiated = QueueExecutor(queue: .global(qos: .userInitiated))
+    fileprivate static let globalUserInteractive = QueueExecutor(queue: .global(qos: .userInteractive))
+    fileprivate static let globalUnspecified = QueueExecutor(queue: .global(qos: .unspecified))
 }
 
 @available(macOS 15.0, iOS 18.0, watchOS 11.0, tvOS 18.0, visionOS 2.0, *)
 extension TaskExecutor where Self == QueueExecutor {
     public static var mainQueue: QueueExecutor {
-        QueueExecutor(queue: .main)
+        QueueExecutor.main
     }
 
     public static func globalQueue(qos: DispatchQoS.QoSClass = .default) -> QueueExecutor {
-        QueueExecutor(queue: .global(qos: qos))
+        switch qos {
+        case .background:
+            return .globalBackground
+        case .utility:
+            return .globalUtility
+        case .default:
+            return .globalDefault
+        case .userInitiated:
+            return .globalUserInitiated
+        case .userInteractive:
+            return .globalUserInteractive
+        case .unspecified:
+            return .globalUnspecified
+        @unknown default:
+            return .init(queue: .global(qos: qos))
+        }
     }
 
     public static func queue(label: String,
--- Tests/AfluentTests/QueueExecutorTests.swift
+++ Tests/AfluentTests/QueueExecutorTests.swift
@@ -26,5 +26,54 @@ struct QueueExecutorTests {
             dispatchPrecondition(condition: .onQueue(queue))
         }.value
     }
+
+    @available(macOS 15.0, iOS 18.0, watchOS 11.0, tvOS 18.0, visionOS 2.0, *)
+    @Test func hasSharedMainQueueInstance() {
+        let executor1 = QueueExecutor.mainQueue
+        let executor2 = QueueExecutor.mainQueue
+        #expect(executor1 === executor2)
+    }
+
+    @available(macOS 15.0, iOS 18.0, watchOS 11.0, tvOS 18.0, visionOS 2.0, *)
+    @Test(arguments: [
+        DispatchQoS.QoSClass.background, .default, .unspecified, .userInitiated, .userInteractive,
+    ]) func hasSharedGlobalQOSInstances(qos: DispatchQoS.QoSClass) {
+        let executor1 = QueueExecutor.globalQueue(qos: qos)
+        let executor2 = QueueExecutor.globalQueue(qos: qos)
+        #expect(executor1 === executor2)
+    }
+
+    @available(macOS 15.0, iOS 18.0, watchOS 11.0, tvOS 18.0, visionOS 2.0, *)
+    @Test func forwardsTaskLocalsWhenUsed() async throws {
+        enum Context {
+            @TaskLocal static var value: String?
+        }
+
+        let expectedValue = UUID().uuidString
+
+        #expect(Context.value == nil)
+
+        Context.$value.withValue(expectedValue) {
+            Task(executorPreference: .mainQueue) {
+                dispatchPrecondition(condition: .onQueue(.main))
+                #expect(Context.value == expectedValue)
+            }
+        }
+
+        Context.$value.withValue(expectedValue) {
+            Task(executorPreference: .globalQueue(qos: .background)) {
+                dispatchPrecondition(condition: .onQueue(.global(qos: .background)))
+                #expect(Context.value == expectedValue)
+            }
+        }
+
+        let queue = DispatchQueue(label: "\(String(describing: Self.self))\(UUID().uuidString)")
+        Context.$value.withValue(expectedValue) {
+            Task(executorPreference: .queue(queue)) {
+                dispatchPrecondition(condition: .onQueue(queue))
+                #expect(Context.value == expectedValue)
+            }
+        }
+    }
 }
 #endif

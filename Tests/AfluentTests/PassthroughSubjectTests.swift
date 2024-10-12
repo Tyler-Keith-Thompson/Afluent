e3f51fb5706a183438ea70ec36b45440a980dcb0
Tyler Thompson <tyler.keith.thompson@gmail.com>
2024-10-12 10:40:00 -0600

    [PassthroughSubject] - Subject stops emmitting values if it has completed without screwing up any current subscribers - TT
    Co-authored-by: Tyler Thompson <tyler.thompson@wwt.com>

--- Tests/AfluentTests/PassthroughSubjectTests.swift
+++ Tests/AfluentTests/PassthroughSubjectTests.swift
@@ -90,47 +90,56 @@ struct PassthroughSubjectTests {
 
         let v = await test.values
         let v2 = await test2.values
-        print(v)
-        print(v2)
+
         #expect(await test.values == [1, 2, 3])
         #expect(await test2.values == [1, 2, 3])
     }
     
-//    @Test func passthroughSubjectStopsSendingValuesUponFinish() async throws {
-//        actor Test {
-//            var values = [Int]()
-//            func appendValue(_ value: Int) {
-//                values.append(value)
-//            }
-//        }
-//        let test = Test()
-//        let subject = PassthroughSubject<Int>()
-//        
-//        let task = Task {
-//            for try await value in subject {
-//                await test.appendValue(value)
-//            }
-//        }
-//        
-//        #expect(await test.values.isEmpty)
-//        
-//        subject.send(1)
-//        subject.send(2)
-//        subject.send(3)
-//        subject.send(completion: .finished)
-//        
-//        _ = try await task.value
-//        
-//        let task2 = Task {
-//            for try await value in subject {
-//                await test.appendValue(value)
-//            }
-//        }
-//        
-//        subject.send(4)
-//        
-//        _ = try await task2.value
-//
-//        #expect(await test.values == [1, 2, 3])
-//    }
+    @Test func passthroughSubjectStopsSendingValuesUponFinish() async throws {
+        actor Test {
+            var values = [Int]()
+            func appendValue(_ value: Int) {
+                values.append(value)
+            }
+        }
+        let test = Test()
+        let subject = PassthroughSubject<Int>()
+        let taskStartedSubject = SingleValueSubject<Void>()
+
+        let task = Task {
+            for try await value in subject.handleEvents(receiveMakeIterator: {
+                try? taskStartedSubject.send()
+            }) {
+                await test.appendValue(value)
+            }
+        }
+        
+        try await taskStartedSubject.execute()
+
+        #expect(await test.values.isEmpty)
+        
+        subject.send(1)
+        subject.send(2)
+        subject.send(3)
+        subject.send(completion: .finished)
+        
+        _ = try await task.value
+        
+        let task2StartedSubject = SingleValueSubject<Void>()
+        let task2 = Task {
+            for try await value in subject.handleEvents(receiveMakeIterator: {
+                try? task2StartedSubject.send()
+            }) {
+                await test.appendValue(value)
+            }
+        }
+
+        try await task2StartedSubject.execute()
+
+        subject.send(4)
+        
+        _ = try await task2.value
+
+        #expect(await test.values == [1, 2, 3])
+    }
 }

024c8a63dbd99a7b051bd70c2548c8224ef7e893
Tyler Thompson <tyler.keith.thompson@gmail.com>
2024-10-12 15:41:22 -0600

    [PassthroughSubject] - Added the rest of the expected functionality - TT
    Co-authored-by: Tyler Thompson <tyler.thompson@wwt.com>

--- Sources/Afluent/PassthroughSubject.swift
+++ Sources/Afluent/PassthroughSubject.swift
@@ -8,8 +8,17 @@
 import Atomics
 
 @_spi(Experimental) public final class PassthroughSubject<Element: Sendable>: AsyncSequence, @unchecked Sendable {
+    private class State: @unchecked Sendable {
+        private let lock = Lock.allocate()
+        private var _finishedResult: Result<Element?, any Error>?
+        var finishedResult: Result<Element?, any Error>? {
+            get { lock.withLock { _finishedResult } }
+            set { lock.withLockVoid { _finishedResult = newValue } }
+        }
+    }
     private let continuation: AsyncThrowingStream<Element, any Error>.Continuation
     private let streamIterator: () -> AsyncBroadcastSequence<AsyncThrowingStream<Element, any Error>>.AsyncIterator
+    private let state = State()
     private let finished: ManagedAtomic<Bool> = .init(false)
     
     public init() {
@@ -21,33 +30,37 @@ import Atomics
     
     public struct Iterator: AsyncIteratorProtocol, @unchecked Sendable {
         var upstream: AsyncBroadcastSequence<AsyncThrowingStream<Element, any Error>>.AsyncIterator
-        let finished: Bool
+        let finished: Result<Element?, any Error>?
         
         public mutating func next() async throws -> Element? {
-            guard !finished else { return nil }
+            guard finished == nil else { return try finished?.get() }
             return try await upstream.next()
         }
     }
     
     public func makeAsyncIterator() -> Iterator {
-        .init(upstream: streamIterator(), finished: finished.load(ordering: .sequentiallyConsistent))
-//        streamIterator()
+        .init(upstream: streamIterator(), finished: state.finishedResult)
     }
     
     public func send(_ element: Element) {
-        guard !finished.load(ordering: .sequentiallyConsistent) else { return }
+        guard state.finishedResult == nil else { return }
         continuation.yield(element)
     }
     
     public func send() where Element == Void {
-        
+        guard state.finishedResult == nil else { return }
+        continuation.yield()
     }
     
     public func send(completion: Completion<any Error>) {
-        defer { finished.store(true, ordering: .sequentiallyConsistent) }
+        guard state.finishedResult == nil else { return }
         switch completion {
-        case .finished: continuation.finish()
-        case .failure(let error): break
+        case .finished:
+            defer { state.finishedResult = .success(nil) }
+            continuation.finish()
+        case .failure(let error):
+            defer { state.finishedResult = .failure(error) }
+            continuation.finish(throwing: error)
         }
     }
 }
--- Tests/AfluentTests/PassthroughSubjectTests.swift
+++ Tests/AfluentTests/PassthroughSubjectTests.swift
@@ -142,4 +142,104 @@ struct PassthroughSubjectTests {
 
         #expect(await test.values == [1, 2, 3])
     }
+    
+    @Test func passthroughSubjectCanCompleteWithError() async throws {
+        enum Err: Error, Equatable {
+            case e1
+        }
+        
+        actor Test {
+            var values = [Int]()
+            func appendValue(_ value: Int) {
+                values.append(value)
+            }
+        }
+        let test = Test()
+        let subject = PassthroughSubject<Int>()
+        let taskStartedSubject = SingleValueSubject<Void>()
+
+        let task = Task {
+            for try await value in subject {
+                try? taskStartedSubject.send()
+                await test.appendValue(value)
+            }
+        }
+        
+        #expect(await test.values.isEmpty)
+        
+        subject.send(1)
+        try await taskStartedSubject.execute()
+        subject.send(completion: .failure(Err.e1))
+        subject.send(3)
+        subject.send(completion: .finished)
+        
+        let task1Result = await task.result
+        try #require(throws: Err.e1, performing: { try task1Result.get() })
+        
+        let task2StartedSubject = SingleValueSubject<Void>()
+        let task2 = Task {
+            for try await value in subject.handleEvents(receiveMakeIterator: {
+                try? task2StartedSubject.send()
+            }) {
+                await test.appendValue(value)
+            }
+        }
+
+        try await task2StartedSubject.execute()
+
+        subject.send(4)
+        
+        let task2Result = await task2.result
+        try #require(throws: Err.e1, performing: { try task2Result.get() })
+
+        #expect(await test.values == [1])
+    }
+    
+    @Test func passthroughSubjectCanSendVoidValues() async throws {
+        actor Test {
+            var count = 0
+            func appendValue() {
+                count += 1
+            }
+        }
+        let test = Test()
+        let subject = PassthroughSubject<Void>()
+        let taskStartedSubject = SingleValueSubject<Void>()
+
+        let task = Task {
+            for try await _ in subject.handleEvents(receiveMakeIterator: {
+                try? taskStartedSubject.send()
+            }) {
+                await test.appendValue()
+            }
+        }
+        
+        try await taskStartedSubject.execute()
+
+        #expect(await test.count == 0)
+        
+        subject.send()
+        subject.send()
+        subject.send()
+        subject.send(completion: .finished)
+        
+        _ = try await task.value
+        
+        let task2StartedSubject = SingleValueSubject<Void>()
+        let task2 = Task {
+            for try await _ in subject.handleEvents(receiveMakeIterator: {
+                try? task2StartedSubject.send()
+            }) {
+                await test.appendValue()
+            }
+        }
+
+        try await task2StartedSubject.execute()
+
+        subject.send()
+        
+        _ = try await task2.value
+
+        #expect(await test.count == 3)
+    }
 }

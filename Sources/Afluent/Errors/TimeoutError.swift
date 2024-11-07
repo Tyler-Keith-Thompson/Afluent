b15b005d3a18a0bcb1c2cca9d94e519bb28ee082
Annalise Mariottini <annalise.mariottini@columbia.edu>
2024-11-07 18:10:23 -0500

    Build fixes (non macOS) (#148)
    * fix iOS build
    
    * fix Xcode 15.4 build
    
    * remove @_spi(Experimental) usage
    
    * use PAT for release

--- Sources/Afluent/Errors/TimeoutError.swift
+++ Sources/Afluent/Errors/TimeoutError.swift
@@ -10,6 +10,7 @@ import Foundation
 /// An error indicating a timeout has occurred.
 ///
 /// Potentially thrown by ``AsynchronousUnitOfWork/timeout(_:customError:)`` and ``AsynchronousUnitOfWork/timeout(_:clock:tolerance:customError:)``.
+@available(macOS 13.0, iOS 16.0, watchOS 9.0, tvOS 16.0, *)
 public enum TimeoutError: Swift.Error, LocalizedError {
     /// A timeout occurred, with the duration timeout.
     ///
@@ -23,15 +24,16 @@ public enum TimeoutError: Swift.Error, LocalizedError {
 
     public var errorDescription: String? {
         switch self {
-        case .timedOut(let duration): return "Timed out after waiting \(duration)"
+            case .timedOut(let duration): return "Timed out after waiting \(duration)"
         }
     }
 }
 
+@available(macOS 13.0, iOS 16.0, watchOS 9.0, tvOS 16.0, *)
 extension TimeoutError: Equatable {
     public static func == (lhs: Self, rhs: Self) -> Bool {
         switch (lhs, rhs) {
-        case (.timedOut, .timedOut): return true
+            case (.timedOut, .timedOut): return true
         }
     }
 }

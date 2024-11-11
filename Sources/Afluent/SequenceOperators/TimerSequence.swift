5b217b7ef90eda864f4a04c5770a2765943a70f9
Annalise Mariottini <annalise.mariottini@columbia.edu>
2024-11-11 10:08:22 -0500

    Add TimerSequence type (#150)
    * Add TimerSequence type
    
    * Fix DocC
    
    * Improve wait until condition
    
    * Fix typo
    
    * Changes to TimerSequence based on MR feedback and Combine behavior
    
    * remove unnecessary messages from test expectations
    
    * remove print statement
    
    * [timer-publisher] - Added a yield to waitUntil to fix a flake and to encourage other work to happen. This makes the sleeping imprecise, but that's likely fine for this kind of utility - TT
    
    Co-authored-by: Tyler Thompson <tyler.thompson@wwt.com>
    
    * [timer-publisher] - Test stability fixes - TT
    
    Co-authored-by: Tyler Thompson <tyler.thompson@wwt.com>
    
    * move waitUntilScheduled into a separate file
    
    ---------
    
    Co-authored-by: Tyler Thompson <tyler.keith.thompson@gmail.com>
    Co-authored-by: Tyler Thompson <tyler.thompson@wwt.com>

---
+++ Sources/Afluent/SequenceOperators/TimerSequence.swift
@@ -0,0 +1,94 @@
+//
+//  TimerSequence.swift
+//  Afluent
+//
+//  Created by Annalise Mariottini on 11/9/24.
+//
+
+import Foundation
+
+@available(macOS 13.0, iOS 16.0, watchOS 9.0, tvOS 16.0, visionOS 1.0, *)
+extension AsyncSequences {
+    /// A sequence that repeatedly emits an instant on a given interval.
+    public struct TimerSequence<C: Clock>: AsyncSequence, Sendable {
+        public typealias Element = C.Instant
+
+        init(interval: C.Duration, tolerance: C.Duration?, clock: C) {
+            self.interval = interval
+            self.tolerance = tolerance
+            self.clock = clock
+        }
+
+        private let interval: C.Duration
+        private let tolerance: C.Duration?
+        private let clock: C
+
+        public struct AsyncIterator: AsyncIteratorProtocol {
+            private var cancellables: Set<AnyCancellable> = []
+
+            init(interval: C.Duration, tolerance: C.Duration?, clock: C) {
+                self.interval = interval
+                self.tolerance = tolerance
+                self.clock = clock
+            }
+
+            private let interval: C.Duration
+            private let tolerance: C.Duration?
+            private let clock: C
+            private var last: C.Instant?
+            private var finished = false
+
+            public mutating func next() async -> C.Instant? {
+                guard !finished else {
+                    return nil
+                }
+                let next = (self.last ?? clock.now).advanced(by: self.interval)
+                do {
+                    try await clock.sleep(until: next, tolerance: self.tolerance)
+                } catch {
+                    self.finished = true
+                    return nil
+                }
+                let now = clock.now
+                self.last = next
+                return now
+            }
+        }
+
+        public func makeAsyncIterator() -> AsyncIterator {
+            AsyncIterator(interval: interval, tolerance: tolerance, clock: clock)
+        }
+    }
+}
+
+@available(macOS 13.0, iOS 16.0, watchOS 9.0, tvOS 16.0, visionOS 1.0, *)
+public typealias TimerSequence = AsyncSequences.TimerSequence
+
+@available(macOS 13.0, iOS 16.0, watchOS 9.0, tvOS 16.0, visionOS 1.0, *)
+extension TimerSequence where C == ContinuousClock {
+    /// Returns a sequence that repeatedly emits an instant of a continuous clock on the given interval.
+    ///
+    /// - Parameters:
+    ///   - interval: The time interval on which to publish events. For example, a value of `.milliseconds(1)` will publish an event approximately every 0.01 seconds.
+    ///   - tolerance: The allowed timing variance when emitting events. Defaults to `nil`, which will schedule with the default tolerance strategy.
+    public static func publish(every interval: C.Duration, tolerance: C.Duration? = nil)
+        -> AsyncSequences.TimerSequence<C>
+    {
+        AsyncSequences.TimerSequence(interval: interval, tolerance: tolerance, clock: .init())
+    }
+}
+
+@available(macOS 13.0, iOS 16.0, watchOS 9.0, tvOS 16.0, visionOS 1.0, *)
+extension TimerSequence {
+    /// Returns a sequence that repeatedly emits an instant of the passed clock on the given interval.
+    ///
+    /// - Parameters:
+    ///   - interval: The time interval on which to publish events. For example, a value of `.milliseconds(1)` will publish an event approximately every 0.01 seconds.
+    ///   - tolerance: The allowed timing variance when emitting events. Defaults to `nil`, which will schedule with the default tolerance strategy.
+    ///   - clock: The clock instance to utilize for sequence timing. For example, `ContinuousClock` or `SuspendingClock`.
+    public static func publish(every interval: C.Duration, tolerance: C.Duration? = nil, clock: C)
+        -> AsyncSequences.TimerSequence<C>
+    {
+        AsyncSequences.TimerSequence(interval: interval, tolerance: tolerance, clock: clock)
+    }
+}
---
+++ Tests/AfluentTests/WaitUntilCondition.swift
@@ -0,0 +1,33 @@
+//
+//  WaitUntilCondition.swift
+//  Afluent
+//
+//  Created by Annalise Mariottini on 11/9/24.
+//
+
+import Afluent
+
+/// Waits for some condition before proceeding, unless the specified timeout is reached, in which case an error is thrown.
+func wait(until condition: @autoclosure @escaping @Sendable () async -> Bool, timeout: Duration)
+    async throws
+{
+    try await wait(until: await condition(), timeout: timeout, clock: ContinuousClock())
+}
+
+/// Waits for some condition before proceeding, unless the specified timeout is reached, in which case an error is thrown.
+func wait<C: Clock>(
+    until condition: @autoclosure @escaping @Sendable () async -> Bool, timeout: C.Duration,
+    clock: C
+) async throws {
+    let start = clock.now
+    let checkTimeout = {
+        if start.duration(to: clock.now) >= timeout {
+            throw TimeoutError.timedOut(duration: timeout)
+        }
+    }
+    while await condition() == false {
+        await Task.yield()
+        try checkTimeout()
+        try await clock.sleep(for: clock.minimumResolution)
+    }
+}

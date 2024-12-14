cc9f6165ef55c1aa60700a180f7b060f1094cee2
Annalise Mariottini <annalise.mariottini@columbia.edu>
2024-12-14 13:59:35 -0500

    fix building for non-macOS platforms with availability annotations (#161)

--- Sources/AfluentTesting/WaitUntilCondition.swift
+++ Sources/AfluentTesting/WaitUntilCondition.swift
@@ -8,6 +8,7 @@
 import Afluent
 
 /// Waits for some condition before proceeding, unless the specified timeout is reached, in which case an error is thrown.
+@available(macOS 13.0, iOS 16.0, watchOS 9.0, tvOS 16.0, *)
 public func wait(
     until condition: @autoclosure @escaping @Sendable () async -> Bool, timeout: Duration
 )
@@ -17,6 +18,7 @@ public func wait(
 }
 
 /// Waits for some condition before proceeding, unless the specified timeout is reached, in which case an error is thrown.
+@available(macOS 13.0, iOS 16.0, watchOS 9.0, tvOS 16.0, *)
 public func wait<C: Clock>(
     until condition: @autoclosure @escaping @Sendable () async -> Bool, timeout: C.Duration,
     clock: C
